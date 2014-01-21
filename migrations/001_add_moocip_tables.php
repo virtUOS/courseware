38c8971ed67aa897ccbfff9b20fa6d873c6852ac
tgloeggl <tgloeggl@uos.de>
2014-01-21 12:25:09 +0100

    add database-scheme to migration

--- migrations/001_add_moocip_tables.php
+++ migrations/001_add_moocip_tables.php
@@ -2,14 +2,37 @@
 class AddMoocipTables extends DBMigration {
 
     public function description () {
-        // ...
+        return 'create tables for the mooc-plugin';
     }
 
     public function up () {
-        // ...
+        $db = DBManager::get();
+        $db->exec("CREATE  TABLE `mooc_blocks` (
+            `id` INT NOT NULL ,
+            `type` VARCHAR(64) NULL ,
+            `parent_id` VARCHAR(32) NULL ,
+            `seminar_id` VARCHAR(32) NULL ,
+            `title` VARCHAR(255) NULL ,
+            `position` INT NULL DEFAULT 0 ,
+            `data` MEDIUMTEXT NULL COMMENT 'JSON' ,
+            `chdate` INT NULL ,
+            `mkdate` INT NULL ,
+            PRIMARY KEY (`id`)
+        )");
+        
+        $db->exec("CREATE  TABLE `mooc_userprogress` (
+            `id` INT NOT NULL ,
+            `block_id` VARCHAR(32) NULL ,
+            `user_id` VARCHAR(32) NULL ,
+            `data` MEDIUMTEXT NULL COMMENT 'JSON' ,
+            `grade` VARCHAR(45) NULL ,
+            `max_grade` VARCHAR(45) NULL ,
+            PRIMARY KEY (`id`)
+        )");
     }
     
     public function down () {
-        // ...
+        DBManager::get()->exec("DROP TABLE mooc_blocks");
+        DBManager::get()->exec("DROP TABLE mooc_userprogress");
     }
 }
