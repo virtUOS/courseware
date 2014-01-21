43b5c6bfc42e53b184b642b5857e22e8a6958d49
luniki <luniki@gmail.com>
2014-01-21 12:19:35 +0100

    cleanup plugin

--- Mooc.php
+++ Mooc.php
@@ -6,7 +6,7 @@
  * ...
  *
  * @author  virtuos
- * @version 0.1a
+ * @version 0.1.0
  */
 
 class Mooc extends StudIPPlugin implements StandardPlugin, SystemPlugin
@@ -15,33 +15,35 @@ class Mooc extends StudIPPlugin implements StandardPlugin, SystemPlugin
     public function __construct() {
         parent::__construct();
 
-        $navigation = new AutoNavigation(_('Mooc.IP'));
-        $navigation->setURL(PluginEngine::GetURL($this, array(), 'show'));
-        $navigation->setImage(Assets::image_path('blank.gif'));
-        Navigation::addItem('/moocip', $navigation);
+        $this->setupNavigation();
     }
 
+    // bei Aufruf des Plugins über plugin.php/mooc/...
     public function initialize ()
     {
         PageLayout::addStylesheet($this->getPluginURL().'/assets/style.css');
         PageLayout::addScript($this->getPluginURL().'/assets/application.js');
     }
 
+    // für Veranstaltungskategorien-Slots
     public function getTabNavigation($course_id)
     {
         return array();
     }
 
+    // ???
     public function getNotificationObjects($course_id, $since, $user_id)
     {
         return array();
     }
 
+    // Icon auf meine_seminare.php
     public function getIconNavigation($course_id, $last_visit, $user_id)
     {
         // ...
     }
 
+    // Box auf der Übersichtsseite
     public function getInfoTemplate($course_id)
     {
         // ...
@@ -63,6 +65,10 @@ class Mooc extends StudIPPlugin implements StandardPlugin, SystemPlugin
         $dispatcher->dispatch($unconsumed_path);
     }
 
+    /**********************************************************************/
+    /* PRIVATE METHODS                                                    */
+    /**********************************************************************/
+
     private function setupAutoload()
     {
         if (class_exists("StudipAutoloader")) {
@@ -72,5 +78,31 @@ class Mooc extends StudIPPlugin implements StandardPlugin, SystemPlugin
                 include_once __DIR__ . $class . '.php';
             });
         }
+    }
+
+    private function setupNavigation()
+    {
+        global $perm;
+
+        $cid = $this->getContext();
+        if (Request::isXhr()
+            || Navigation::hasItem('/course/mooc')
+            || !$this->isActivated($cid))
+        {
+            return;
+        }
+
+        $url = PluginEngine::getURL($this, compact('cid'), 'TODO', true);
+
+        $navigation = new Navigation(_('Mooc'), $url);
+        $navigation->setImage(Assets::image_path('icons/16/white/category.png'));
+        $navigation->setActiveImage(Assets::image_path('icons/16/black/category.png'));
+
+        Navigation::addItem('/course/mooc', $navigation);
+    }
+
+    private function getContext()
+    {
+        return Request::option("cid");
     }
 }
