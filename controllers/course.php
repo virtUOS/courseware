f0528cfbf16797191986cf83f1fa9915956fc785
luniki <luniki@gmail.com>
2014-01-23 13:58:33 +0100

    use models in controllers

--- controllers/course.php
+++ controllers/course.php
@@ -7,11 +7,13 @@ class CourseController extends MoocipController {
     public function before_filter(&$action, &$args)
     {
         parent::before_filter($action, $args);
+        $this->cid = $this->plugin->getContext();
     }
 
     public function index_action()
     {
         Navigation::activateItem("/course/mooc_overview");
-        $this->answer = 'yes';
+
+        $this->courseware = \Mooc\Courseware::findByCourse($this->cid);
     }
 }
--- controllers/courseware.php
+++ controllers/courseware.php
@@ -7,10 +7,12 @@ class CoursewareController extends MoocipController {
     public function before_filter(&$action, &$args)
     {
         parent::before_filter($action, $args);
+        $this->cid = $this->plugin->getContext();
     }
 
     public function index_action()
     {
         Navigation::activateItem("/course/mooc_courseware");
+        $this->courseware = \Mooc\Courseware::findByCourse($this->cid);
     }
 }
