2f715b262f862d0c2801654f73ab0dcbe4b246f7
tgloeggl <tgloeggl@uos.de>
2014-02-04 12:42:37 +0100

    add migration for mooc_fields

---
+++ migrations/005_add_fields_table.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @author Till Glöggler <tgloeggl@uos.de>
+ */
+class ModifyBlockTypeColumn extends Migration
+{
+
+    function description()
+    {
+        return 'Don\'t allow null values in the type column.';
+    }
+
+    function up()
+    {
+        $db = DBManager::get();
+        $db->exec('CREATE  TABLE IF NOT EXISTS `mooc_fields` (
+            `block_id` INT NOT NULL ,
+            `user_id` VARCHAR(32) NULL ,
+            `name` VARCHAR(255) NOT NULL ,
+            `json_data` MEDIUMTEXT NULL ,
+            PRIMARY KEY (`block_id`, `user_id`, `name`)
+        )');
+    }
+
+    function down()
+    {
+        $db = DBManager::get();
+        $db->exec('DROP TABLE mooc_fields');
+    }
+}
