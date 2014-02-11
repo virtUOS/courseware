b08a4d1ee9a6ad8a1ba0ae1f618ced3cce936992
luniki <luniki@gmail.com>
2014-02-11 14:50:55 +0100

    start with client side code (JS), refs #19

--- views/courseware/index.php
+++ views/courseware/index.php
@@ -1,20 +1,69 @@
 <?
 $body_id = 'mooc-courseware-index';
+$ASSETS = $plugin->getPluginURL() . '/assets/';
 ?>
 
-<h1> Courseware: <?= htmlReady($courseware->title) ?></h1>
+<section id=courseware>
 
-<ol class=chapters>
-<?= $this->render_partial_collection('courseware/_chapter', $courseware->chapters) ?>
-</ol>
+  <h1> Courseware: <?= htmlReady($courseware->title) ?></h1>
 
-<ol class=sections>
-<?= $this->render_partial_collection('courseware/_section', $subchapter->sections) ?>
-</ol>
+  <ol class=chapters>
+    <?= $this->render_partial_collection('courseware/_chapter', $courseware->chapters) ?>
+  </ol>
 
-<section class=content>
-  <p>Diese Section hat <?= count($section->blocks) ?> Block/s.</p>
+  <ol class=sections>
+    <?= $this->render_partial_collection('courseware/_section', $subchapter->sections) ?>
+  </ol>
 
-  <?= $this->render_partial_collection('courseware/_block', $section->blocks) ?>
+  <section class=content>
+    <p>Diese Section hat <?= count($section->blocks) ?> Block/s.</p>
+
+    <?= $this->render_partial_collection('courseware/_block', $section->blocks) ?>
+
+  </section>
 
 </section>
+
+
+<script>
+
+ <?
+ $block_types = array_map("basename", glob($plugin->getPluginPath() . '/blocks/*'));
+ $block_deps  = array_map(function ($type) { return "\"blocks/$type/js/$type\""; }, $block_types);
+ $blocks_url  = current(explode("?", $controller->url_for("blocks")));
+ ?>
+
+
+ var require = {
+
+   baseUrl: "<?= $plugin->getPluginURL()?>",
+
+   paths: {
+     domReady: "assets/js/domReady",
+     backbone: "assets/js/vendor/backbone/backbone-min"
+   },
+
+   shim: {
+     backbone: {
+       exports: 'Backbone'
+     }
+   },
+
+   deps: ["domReady!", "assets/js/courseware", <?= join(",", $block_deps) ?>],
+
+   callback: function(domReady, Courseware) {
+     var courseware = new Courseware({el: document.getElementById('courseware')});
+   },
+
+   config: {
+     "assets/js/courseware": {
+       block_types: <?= json_encode(studip_utf8decode($block_types)) ?>
+     },
+
+     "assets/js/url": {
+       blocks_url: <?= json_encode(studip_utf8decode($blocks_url)) ?>
+     }
+   }
+ };
+</script>
+<script src="<?= $ASSETS ?>js/require.js"></script>
