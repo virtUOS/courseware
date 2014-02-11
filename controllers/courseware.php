2f006d5f231de7c289cd2cba6f1d0347780d9c19
luniki <luniki@gmail.com>
2014-02-11 14:48:06 +0100

    dynamicize courseware/index route, refs #19

--- controllers/courseware.php
+++ controllers/courseware.php
@@ -7,21 +7,25 @@ class CoursewareController extends MoocipController {
     public function before_filter(&$action, &$args)
     {
         parent::before_filter($action, $args);
-        $this->cid = $this->plugin->getContext();
     }
 
     public function index_action()
     {
         Navigation::activateItem("/course/mooc_courseware");
 
-        $this->selected = 4;
-
-        $this->courseware = \Mooc\Courseware::findByCourse($this->cid);
+        $this->courseware = \Mooc\Courseware::findByCourse($this->container['cid']);
 
         // TODO: verify section is in courseware & we have access to it
         // TODO: Section::find finds any block not only Sections. Ouch!
-        $this->section    = \Mooc\Section::find($this->selected);
 
+
+        // TODO
+        $this->section = current(
+            \Mooc\AbstractBlock::findBySQL(
+                'seminar_id = ? AND type = "Section" ORDER BY parent_id, position',
+                array($this->container['cid'])));
+
+        $this->selected   = $this->section->id;
         $this->subchapter = $this->section->subchapter;
         $this->chapter    = $this->subchapter->chapter;
     }
