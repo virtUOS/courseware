083f17f2f9c1c64676e55a2de1bc37b82aaae39a
luniki <luniki@gmail.com>
2014-02-11 14:49:55 +0100

    add additional data to block partial, refs #19

--- views/courseware/_block.php
+++ views/courseware/_block.php
@@ -2,7 +2,13 @@
 $id = intval($_block->id);
 $ui_block = $this->container['block_factory']->makeBlock($_block);
 ?>
-<section id=block-<?= $id ?> class="block <?= htmlReady($_block->type) ?>">
+<section
+id=block-<?= $id ?>
+    class="block <?= htmlReady($_block->type) ?>"
+    data-id="<?= htmlReady($_block->id) ?>"
+    data-type="<?= htmlReady($_block->type) ?>">
+
   <h1>Block No. <?= $id ?></h1>
+
   <?= $this->container['block_renderer']($ui_block, 'student') ?>
 </section>
