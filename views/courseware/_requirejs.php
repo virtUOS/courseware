da1156fb5267f4de5d5106131319a5d9732f95af
luniki <luniki@gmail.com>
2014-02-22 13:48:51 +0100

    refactor views/courseware/index

---
+++ views/courseware/_requirejs.php
@@ -0,0 +1,51 @@
+<script>
+
+ <?
+ $block_types = array_map("basename", glob($plugin->getPluginPath() . '/blocks/*'));
+ $blocks_url  = current(explode("?", $controller->url_for("blocks")));
+ ?>
+
+ var require = {
+
+   config: {
+
+     "block": {
+       block_types: <?= json_encode(studip_utf8decode($block_types)) ?>
+     },
+
+     "assets/js/url": {
+       blocks_url: <?= json_encode(studip_utf8decode($blocks_url)) ?>,
+       base_view: "<?= $view ?>"
+     },
+
+     "assets/js/templates": {
+       templates: <?= json_encode(studip_utf8decode($templates)) ?>
+     }
+   },
+
+   baseUrl: "<?= $plugin->getPluginURL()?>",
+
+   paths: {
+     domReady: "assets/js/domReady",
+     block:    "assets/js/block_loader",
+     backbone: "assets/js/vendor/backbone/backbone-min",
+     argjs:    "assets/js/vendor/arg.js/arg.js.v1.1"
+   },
+
+   shim: {
+     backbone: {
+       exports: 'Backbone'
+     },
+     argjs: {
+       exports: 'Arg',
+     }
+   },
+
+   deps: ['domReady!', 'block!Courseware'],
+
+   callback: function(domReady, CoursewareViews) {
+     new CoursewareViews.student({ el: $("#courseware") });
+   }
+ };
+</script>
+<script src="<?= $plugin->getPluginURL() . '/assets/' ?>js/require.js"></script>
--- views/courseware/index.php
+++ views/courseware/index.php
@@ -1,51 +1,5 @@
-<?
-$body_id = 'mooc-courseware-index';
-$ASSETS = $plugin->getPluginURL() . '/assets/';
+<? $body_id = 'mooc-courseware-index'; ?>
 
-// TODO: $context missing
-echo $courseware_block->render("student", $context);
-?>
+<?= $courseware_block->render($view, $context) ?>
 
-<script>
-
- <?
- $block_types = array_map("basename", glob($plugin->getPluginPath() . '/blocks/*'));
- $blocks_url  = current(explode("?", $controller->url_for("blocks")));
- ?>
-
-
- var require = {
-
-   config: {
-
-     "block": {
-       block_types: <?= json_encode(studip_utf8decode($block_types)) ?>
-     },
-
-     "assets/js/url": {
-       blocks_url: <?= json_encode(studip_utf8decode($blocks_url)) ?>
-     }
-   },
-
-   baseUrl: "<?= $plugin->getPluginURL()?>",
-
-   paths: {
-     domReady: "assets/js/domReady",
-     block:   "assets/js/block",
-     backbone: "assets/js/vendor/backbone/backbone-min"
-   },
-
-   shim: {
-     backbone: {
-       exports: 'Backbone'
-     }
-   },
-
-   deps: ['domReady!', 'block!Courseware'],
-
-   callback: function(domReady, CoursewareViews) {
-     new CoursewareViews.student({ el: $("#courseware") });
-   }
- };
-</script>
-<script src="<?= $ASSETS ?>js/require.js"></script>
+<?= $this->render_partial('courseware/_requirejs') ?>
