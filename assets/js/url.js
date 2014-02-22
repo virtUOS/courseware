6f119dc6518753fcd8b343b5c358001198ea28b6
luniki <luniki@gmail.com>
2014-02-22 13:48:15 +0100

    add arg.js to generate URLs

--- assets/js/url.js
+++ assets/js/url.js
@@ -1,13 +1,25 @@
-define(['module'], function (module) {
+define(['module', 'argjs'], function (module, Arg) {
 
     'use strict';
 
-    var block_url = function (block_id) {
-        return [module.config().blocks_url, "/", block_id].join("");
-    };
-
-
     return {
+        base_view: module.config().base_view,
+
+        block_url: function (block_id) {
+            return [module.config().blocks_url, "/", block_id].join("");
+        },
+
+        getView: function (block_id, view_name) {
+            var payload = {
+            };
+
+            return $.ajax({
+                url: this.block_url(block_id) + "?view=" + view_name,
+                accepts: "text/html",
+                type: "GET"
+            });
+        },
+
         callHandler: function (block_id, handler, data) {
 
             var payload = {
@@ -16,12 +28,17 @@ define(['module'], function (module) {
             };
 
             return $.ajax({
-                url: block_url(block_id),
+                url: this.block_url(block_id),
                 type: "POST",
                 data: JSON.stringify(payload),
                 contentType: "application/json",
                 dataType: "json"
             });
+        },
+
+        switchToView: function (view) {
+            var url = Arg.url({ cid: Arg('cid'), view: view });
+            window.document.location = url;
         }
     };
 
