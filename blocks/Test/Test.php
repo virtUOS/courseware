df3f17292714d96d1fb7e017172beef55ef9a244
luniki <luniki@gmail.com>
2014-02-27 17:43:06 +0100

    make sure we do not get errors, refs #5

--- blocks/Test/Test.php
+++ blocks/Test/Test.php
@@ -18,29 +18,33 @@ class Test extends Block
     public function initialize()
     {
         $this->defineField('test_id', \Mooc\SCOPE_BLOCK, null);
-        $this->test = new TestModel($this->test_id);
+        if (\PluginEngine::getPlugin('VipsPlugin')) {
+            $this->test = new TestModel($this->test_id);
+        }
     }
 
     public function student_view()
     {
         $exercises = array();
 
-        foreach ($this->test->exercises as $exercise) {
-            require_once __DIR__.'/../../../VipsPlugin/exercises/'.$exercise->URI.'.php';
-            $vipsExercise = new $exercise->URI($exercise->Aufgabe, $exercise->ID);
+        if ($this->test) {
+            foreach ($this->test->exercises as $exercise) {
+                require_once __DIR__.'/../../../VipsPlugin/exercises/'.$exercise->URI.'.php';
+                $vipsExercise = new $exercise->URI($exercise->Aufgabe, $exercise->ID);
 
-            $entry = array(
-                'question' => $vipsExercise->question,
-                'answers' => array(),
-            );
-
-            foreach ($vipsExercise->answerArray[0] as $answer) {
-                $entry['answers'][] = array(
-                    'text' => $answer,
+                $entry = array(
+                    'question' => $vipsExercise->question,
+                    'answers' => array(),
                 );
-            }
 
-            $exercises[] =  $entry;
+                foreach ($vipsExercise->answerArray[0] as $answer) {
+                    $entry['answers'][] = array(
+                        'text' => $answer,
+                    );
+                }
+
+                $exercises[] =  $entry;
+            }
         }
 
         return array(
