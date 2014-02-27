4ff82202470a742261a4ef910a15b7ed9d9218cc
luniki <luniki@gmail.com>
2014-02-27 12:43:15 +0100

    force windows-1252 in the Mustache Renderer, refs #28

--- models/mooc/ui/MustacheRenderer.php
+++ models/mooc/ui/MustacheRenderer.php
@@ -9,7 +9,8 @@ class MustacheRenderer
     public function __invoke($ui_block, $view_name, $template_data) {
         $block_template_dir = $ui_block->getBlockDir() . '/templates/';
         $loader = new \Mustache_Loader_FilesystemLoader($block_template_dir);
-        $m = new \Mustache_Engine(compact('loader'));
+        $charset = 'windows-1252';
+        $m = new \Mustache_Engine(compact('loader', 'charset'));
 
         return $m->render($view_name . '_view', $template_data);
     }
