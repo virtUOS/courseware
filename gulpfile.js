82d8c5d7600a40d5f4dc0876822774c8210d95a6
tgloeggl <tgloeggl@uos.de>
2014-03-13 14:07:33 +0100

    add gulp-based build-process

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp   = require('gulp');
+var sass   = require('gulp-sass');
+var minify = require('gulp-minify-css');
+var concat = require('gulp-concat');
+var zip    = require('gulp-zip');
+var uglify = require('gulp-uglify');
+
+gulp.task('default', ['sass']);
+
+gulp.task('sass', function() {
+    // place code for your default task here
+    return gulp.src(['./blocks/*/css/*.scss', './assets/*.scss'])
+        .pipe(sass())
+        .pipe(minify())
+        .pipe(concat('moocip.min.css'))
+        .pipe(gulp.dest('./assets'));
+});
+
+/*
+gulp.task('js', function() {
+    return gulp.src([])
+        .pipe(uglify())
+        .pipe()
+});
+*/
+
+gulp.task('zip', ['default'], function() {
+    return gulp.src(['./assets/**', './blocks/**', './controllers/**', './migrations/**', './models/**', './vendor/**', './views/**', 'Mooc.php', 'LICENSE', 'README.md'])
+        .pipe(zip('moocip.zip'))
+        .pipe(gulp.dest('.'))
+});
