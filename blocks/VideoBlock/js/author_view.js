e60d64755cb3fb06c1590709a7d472f9423369f1
Robert Costa <zabbarob@gmail.com>
2014-03-17 17:55:09 +0100

    normalize youtube short links; fixes #40

--- blocks/VideoBlock/js/author_view.js
+++ blocks/VideoBlock/js/author_view.js
@@ -2,19 +2,42 @@ define(['assets/js/author_view', 'assets/js/url'], function (
     AuthorView, helper
 ) {
     'use strict';
+
+    function normalizeYouTubeLink(url) {
+        // http://www.youtube.com/watch?v=C3HFAyigqoY&feature=youtu.be
+        // http://youtu.be/C3HFAyigqoY
+        // ==> //www.youtube.com/embed/C3HFAyigqoY
+        var
+        videoId = '\\w*',
+        idQuery = 'v=(' + videoId + ')',
+        queryName = '(?:[^=&;#]{2,}|[^=&;#v])',
+        queryValue = '(?:=[^&;#]*)?',
+        otherQueries = '(?:' + queryName + queryValue + '[&;])*',
+        longLink = '(?:www\\.)?youtube\\.com\\/watch\\?' + otherQueries + idQuery,
+        shortLink = 'youtu\\.be\\/(' + videoId + ')',
+        youTubeLink = '^\\s*'       // ignore whitespace at beginning of line
+        + '(?:https?:)?\\/\\/'  // URL scheme is optional
+        + '(?:' + longLink + '|' + shortLink + ')',
+        matches = url.match(new RegExp(youTubeLink)),
+        id = matches ? (matches[1] || matches[2]) : null;
+        return id ? ('//www.youtube.com/embed/' + id) : url;
+    }
+    function normalizeMatterhornLink(url) {
+        return url;
+    }
+    function normalizeLink(url) {
+        return normalizeMatterhornLink(normalizeYouTubeLink(url));
+    }
+
     return AuthorView.extend({
         events: {
             'click button': function (event) {
-                var view = this,
-                    url = view.$('input').val();
-                // http://www.youtube.com/watch?v=lvB2nRGMl2c
-                var youtube = url.match(/^\s*(?:https?:)?\/\/(?:www\.)?youtube\.com\/watch\?v=(\w*)/);
-                if (youtube) {
-                    url = '//www.youtube.com/embed/' + youtube[1];
-                }
+                var view = this;
 
                 helper
-                .callHandler(view.model.id, 'save', { url: url })
+                .callHandler(view.model.id, 'save', {
+                    url: normalizeLink(view.$('input').val())
+                })
                 .then(function () { // success
                     $(event.target).addClass('accept');
                     view.switchBack();
