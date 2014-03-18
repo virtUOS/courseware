99eb26f732e86c34d30b8f648431f55577abc3fd
luniki <luniki@gmail.com>
2014-03-18 15:15:20 +0100

    add watch task

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,20 @@ var minify = require('gulp-minify-css');
 var concat = require('gulp-concat');
 var zip    = require('gulp-zip');
 
+var paths = {
+    styles: ['./blocks/*/css/*.scss', './assets/*.scss']
+};
+
+
 gulp.task('default', ['sass']);
 
 gulp.task('sass', function() {
     // place code for your default task here
-    return gulp.src(['./blocks/*/css/*.scss', './assets/*.scss'])
-        .pipe(sass())
+    return gulp.src(paths.styles)
+        .pipe(sass()).pipe(gulp.dest('blocks'))/*
         .pipe(minify())
         .pipe(concat('moocip.min.css'))
-        .pipe(gulp.dest('./assets'));
+        .pipe(gulp.dest('./assets'))*/;
 });
 
 /*
@@ -26,5 +31,10 @@ gulp.task('js', function() {
 gulp.task('zip', ['default'], function() {
     return gulp.src(['./assets/**', './blocks/**', './controllers/**', './migrations/**', './models/**', './vendor/**', './views/**', 'Mooc.php', 'LICENSE', 'README.md'])
         .pipe(zip('moocip.zip'))
-        .pipe(gulp.dest('.'))
+        .pipe(gulp.dest('.'));
+});
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+  gulp.watch(paths.styles, ['sass']);
 });
