3e46712f6a50c727a516bd8f1aa50de46a32cdcf
luniki <luniki@gmail.com>
2014-04-11 11:26:15 +0200

    prepare section icons, refs #73

--- blocks/Section/Section.php
+++ blocks/Section/Section.php
@@ -3,9 +3,25 @@ namespace Mooc\UI;
 
 class Section extends Block {
 
+    const ICON_VIDEO   = 'video';
+    const ICON_TASK    = 'task';
+    const ICON_DEFAULT = 'document';
+
+    // definition of precedence of icons
+    // larger array index -> higher precedence
+    // thus ICON_VIDEO has the highest precedence
+    private static $icon_precedences = array(self::ICON_DEFAULT, self::ICON_TASK, self::ICON_VIDEO);
+
+    // mapping of block types to icons
+    private static $map_blocks_to_icons = array(
+        'VideoBlock' => self::ICON_VIDEO,
+        'TestBlock'  => self::ICON_TASK
+    );
+
     function initialize()
     {
         $this->defineField('visited', \Mooc\SCOPE_USER, false);
+        $this->defineField('icon',    \Mooc\SCOPE_BLOCK, self::ICON_DEFAULT);
     }
 
     function student_view($context = array())
@@ -15,7 +31,8 @@ class Section extends Block {
             $this->visited = true;
         }
 
-        $title = $this->title;
+        $icon    = $this->icon;
+        $title   = $this->title;
         $visited = $this->visited;
 
         $blocks = $this->traverseChildren(
@@ -33,7 +50,7 @@ class Section extends Block {
             $content_block_types[] = compact("type");
         }
 
-        return compact('blocks', 'content_block_types', 'title', 'visited');
+        return compact('blocks', 'content_block_types', 'icon', 'title', 'visited');
     }
 
     function add_content_block_handler($data) {
@@ -61,6 +78,8 @@ class Section extends Block {
 
         $block->store();
 
+        $this->updateIconWithBlock($block);
+
         return $block->toArray();
     }
 
@@ -81,6 +100,42 @@ class Section extends Block {
 
         $child->delete();
 
+        $this->refreshIcon();
+
         return array("status" => "ok");
+    }
+
+    private function updateIconWithBlock($new_block)
+    {
+        if (!isset(self::$map_blocks_to_icons[$new_block->type])) {
+            return;
+        }
+
+        $icon_for_block = self::$map_blocks_to_icons[$new_block->type];
+        $precedence = array_search($icon_for_block, self::$icon_precedences);
+        $current_precedence = array_search($this->icon, self::$icon_precedences);
+
+        if ($precedence > $current_precedence) {
+            $this->icon = $icon_for_block;
+        }
+    }
+
+    private function refreshIcon()
+    {
+        $highest_icon = self::ICON_DEFAULT;
+        $highest_precedence = 0;
+
+        foreach ($this->_model->children as $block) {
+            $icon = isset(self::$map_blocks_to_icons[$block->type])
+                ? self::$map_blocks_to_icons[$block->type]
+                : self::ICON_DEFAULT;
+
+            $precedence = array_search($icon, self::$icon_precedences);
+            if ($precedence > $highest_precedence) {
+                $highest_icon       = $icon;
+                $highest_precedence = $precedence;
+            }
+        }
+        $this->icon = $highest_icon;
     }
 }
