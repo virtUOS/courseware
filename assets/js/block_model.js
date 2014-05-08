f3618e2c2e38586b1200d7f03ae1cafab0849d46
luniki <luniki@gmail.com>
2014-05-08 15:26:54 +0200

    add urlRoot to JS BlockModels

--- assets/js/block_model.js
+++ assets/js/block_model.js
@@ -3,11 +3,9 @@ define(['backbone', './url'], function (Backbone, url) {
     'use strict';
 
     var BlockModel = Backbone.Model.extend({
-        /*
         urlRoot: function () {
             return url.block_url("");
         }
-         */
     });
 
 
