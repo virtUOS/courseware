8465ab8102e4ceee042774dc0a8c42b29a3ff0ba
Christian Flothmann <cflothma@uos.de>
2014-05-12 15:48:02 +0200

    update vendor libraries
    - update to mustache/mustache 2.6.0 (was 2.5.1 before)
    - update to willdurand/negotiation 1.3.2 (was 1.3.1 before)

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitc5b574f3618c2f26d25d82ba54b7af71::getLoader();
+return ComposerAutoloaderInit8808b440473c624d5bb839fe21f309d2::getLoader();
