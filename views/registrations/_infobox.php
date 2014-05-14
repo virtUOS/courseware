8530e07d36f0409502da86794521e2ae396edd0d
Tobias Thelen <tobias.thelen@uni-osnabrueck.de>
2014-05-14 08:48:58 +0200

    OHN-Verweis aus Infobox entfernt
    OHN-Verweis aus Infobox entfernt

--- views/registrations/_infobox.php
+++ views/registrations/_infobox.php
@@ -1,4 +1,4 @@
-<p>Sie haben schon einen OHN-Account?</p>
+<p>Sie haben schon einen Account?</p>
 <? if ($GLOBALS['user']->id === 'nobody') : ?>
   <?= $this->render_partial('registrations/_login_and_register') ?>
 <? else :?>
