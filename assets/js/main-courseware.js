6ee61d27b86f30fb4dfc673ce68b865ddb6880cb
luniki <luniki@gmail.com>
2014-06-19 13:48:27 +0200

    alert user of JS errors, refs #207

--- assets/js/main-courseware.js
+++ assets/js/main-courseware.js
@@ -1,5 +1,13 @@
 define(['domReady!', 'scrollTo', 'backbone', 'assets/js/url', 'assets/js/block_types', 'assets/js/block_model'], function (domReady, scrollTo, Backbone, helper, block_types, BlockModel) {
 
+    function logError(error) {
+        alert(error);
+    }
+
+    window.onerror  = function (message, file, line) {
+        logError(file + ':' + line + '\n\n' + message);
+    };
+
     Backbone.history.start({
         push_state: true,
         silent: true,
