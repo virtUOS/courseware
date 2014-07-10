56924c914c8b3e00dabc8afa848d6ec8e35aa4a9
luniki <luniki@gmail.com>
2014-07-10 13:16:09 +0200

    use config.js in build.js

--- build.js
+++ build.js
@@ -3,29 +3,7 @@
 
     baseUrl: ".",
 
-    paths: {
-        domReady: "assets/js/domReady",
-        jquery:   "assets/js/jquery_compat",
-        backbone: "assets/js/vendor/backbone/backbone-min",
-        argjs:    "assets/js/vendor/arg.js/arg.js.v1.1",
-        mustache: "assets/js/vendor/mustache.js-0.8.1/mustache",
-        q:        "assets/js/vendor/q.v1/q.min",
-        scrollTo: "assets/js/vendor/jquery.scrollTo/jquery.scrollTo.min",
-        utils:    "assets/js/utils",
-        dateFormat: "assets/js/vendor/date.format/date.format"
-    },
-
-    shim: {
-        backbone: {
-            exports: 'Backbone'
-        },
-        argjs: {
-            exports: 'Arg'
-        },
-        scrollTo: {
-            deps: ['jquery']
-        }
-    },
+    mainConfigFile: "assets/js/config.js",
 
     include: [
         "assets/js/vendor/requirejs.v2.1.11/require-min"
