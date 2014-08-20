33169e5f012b21dc75b87306d905a85bddbe41f0
Christian Flothmann <christian.flothmann@uos.de>
2014-08-20 16:44:38 +0200

    show information only if they exist, fixes #255

--- views/courses/_show_infobox.php
+++ views/courses/_show_infobox.php
@@ -1,3 +1,11 @@
+<?php
+/** @var string $preview_image */
+/** @var string $preview_video */
+/** @var string $start */
+/** @var string $duration */
+/** @var string $hint */
+?>
+
 <table class="infobox" width="251" cellspacing="0" cellpadding="0">
     <tbody>
         <tr>
@@ -5,20 +13,34 @@
                 <table id="infobox_content" cellspacing="0" cellpadding="4">
                     <tbody>
                         <tr>
-                            <td style="cursor: pointer" id="preview_video">
-                              <div id="preview_image_container">
-                                <img src="<?= $preview_image ?: CourseAvatar::getAvatar($course->id)->getURL(Avatar::NORMAL) ?>">
-			        <div id="play_image"></div>
-			      </div>
+                            <td<?=$preview_video ? 'style="cursor: pointer"' : ''?> id="preview_video">
+                                <div id="preview_image_container">
+                                    <img src="<?= $preview_image ?: CourseAvatar::getAvatar($course->id)->getURL(Avatar::NORMAL) ?>">
+
+                                    <?php
+                                    if ($preview_video):
+                                        echo '<div id="play_image"></div>';
+                                    endif;
+                                    ?>
+                                </div>
                             </td>
                         </tr>
                         <tr>
                             <td>
-                                Start: <?= strftime('%x', strtotime($start)) ?><br>
-                                <br>
-                                Dauer: <?= $duration ?><br>
-                                <br>
-                                <?= formatReady($hint) ?>
+                                <?php
+                                if ($start):
+                                    echo 'Start: '.strftime('%x', strtotime($start));
+                                endif;
+
+                                if ($duration):
+                                    echo '<br>';
+                                    echo 'Dauer: '.$duration.'<br>';
+                                endif;
+
+                                if ($hint):
+                                    echo formatReady($hint);
+                                endif;
+                                ?>
                             </td>
                         </tr>
                     </tbody>
