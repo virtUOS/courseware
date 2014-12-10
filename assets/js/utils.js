37077cc4ab12bc3db0069a5e6dbe62581f808573
Ron Lucke <ronlucke@googlemail.com>
2014-12-10 10:06:25 +0100

    clean up video block code

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,25 +1,16 @@
 define({
-
     getYouTubeId: function(url) {
-        if (url.length == 11) {
-            return url;
-        }
-        var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
-        var match = url.match(regExp);
-
-        if (match && match[7].length == 11) {
-            return match[7];
-        } else {
-            return false;
-        }
+        if(url.length == 11) return url;
+        var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/,
+        match = url.match(regExp);
+        if(match && match[7].length == 11) return match[7];
+        else return false;
     },
-
-    getUrl: function(view, videotype){
+    getUrl: function(view, videotype) {
         var url = '', message = '';
-        switch(videotype){
+        switch(videotype) {
             case 'youtube':
-                var id = view.$('#videosrc'),
-                value = id.val();
+                var id = view.$('#videosrc'), value = id.val();
                 if(this.getYouTubeId(value)) var youtubeid = this.getYouTubeId(value); 
 		else { 
 			message = 'Fehlerhafte youtube ID. Wert wurde zurückgesetzt.';
@@ -30,14 +21,11 @@ define({
                 break;
             case 'matterhorn':
                 var matterhornurl = view.$('#videosrc').val();
-		if (matterhornurl.indexOf('?id=') == -1 ){
+		if(matterhornurl.indexOf('?id=') == -1 ) {
 			message = 'Keine Matterhorn ID übergeben. Wert wurde zurückgesetzt.';
                         break;
                 }
-		if (matterhornurl.indexOf('/engage/ui/watch.html?') != -1){
-			// see https://opencast.jira.com/wiki/display/MH/Engage+URL+Parameters
-		        // http://someURL:8080/engage/ui/watch.html?id=someMediaPackageId
-		        // http://someURL:8080/engage/ui/embed.html?id=someMediaPackageId
+		if(matterhornurl.indexOf('/engage/ui/watch.html?') != -1) {
 			matterhornurl = matterhornurl.replace('/engage/ui/watch.html?', '/engage/ui/embed.html?');
 			message = 'Matterhorn URL wurde berichtigt.';
 		}
@@ -48,13 +36,12 @@ define({
             case 'url':
                 url = view.$('#videosrc').val();
                 break;
-	    }
+	}
 	if(message != '') view.$('.status').html(message).css('color', '#ff0000').fadeIn().delay(3000).fadeOut();
 	else view.$('.status').html(url).css('color', '#24437c').fadeIn().delay(3000).fadeOut();
         return url;
     },
-
-    getVideoType: function(url){
+    getVideoType: function(url) {
 	var videotype = '';
 	if(url.indexOf("youtube") != -1) videotype = "youtube";
 	else if (url.indexOf("engage") != -1) videotype = "matterhorn";
@@ -66,9 +53,9 @@ define({
 	view.$('#videosettings input').val('').removeAttr('checked').removeAttr('selected').prop('disabled', false);
     },
     setVideoData: function(view, url, videotype){
-	if (videotype == 'youtube'){
+	if(videotype == 'youtube') {
 		view.$('#videocontrols').prop('disabled', true);
-		if (this.getVideoType(url) == 'youtube') {
+		if(this.getVideoType(url) == 'youtube') {
 	                var youtubeid = url.slice(29).split("?",1);
                         view.$('#videosrc').val(youtubeid);
                         var start = url.slice(url.indexOf("start=")+6, url.length);
@@ -82,22 +69,22 @@ define({
 			if(parseInt(autoplay) == 1) view.$('#videoautostart').attr("checked", '');
 		}
 	}	
-        if(videotype=='matterhorn'){
+        if(videotype == 'matterhorn'){
 		view.$('#videoendmin').prop('disabled', true);
                 view.$('#videoendsec').prop('disabled', true);
-		if (this.getVideoType(url)== 'matterhorn'){
+		if(this.getVideoType(url) == 'matterhorn') {
 			var urlandid = url.split("&", 1);
 			var autoplay = '', start = '', hidecontrols = '';
 			view.$('#videosrc').val(urlandid);
 			var urlArray = url.split("&");
-			$.each(urlArray, function( index, value){
-				if(value.indexOf('play') != -1)  autoplay = value.split('=')[1];
-				if (value.indexOf('t=') != -1) start = value.split('=')[1];
+			$.each(urlArray, function( index, value) {
+				if(value.indexOf('play') != -1) autoplay = value.split('=')[1];
+				if(value.indexOf('t=') != -1) start = value.split('=')[1];
 				if(value.indexOf('hideControls') != -1) hidecontrols = value.split('=')[1];
 			});
-			if (autoplay == 'true') view.$('#videoautostart').attr("checked", '');
-			if (hidecontrols == 'true') view.$('#videocontrols').attr("checked", '');
-			if (start != ''){ 
+			if(autoplay == 'true') view.$('#videoautostart').attr("checked", '');
+			if(hidecontrols == 'true') view.$('#videocontrols').attr("checked", '');
+			if(start != '') { 
 				var start = start.split("m");
 				view.$('#videostartmin').val(start[0]);
 				view.$('#videostartsec').val(start[1].split("s",1)); 
@@ -106,43 +93,37 @@ define({
 	}
         if(videotype == 'url') {
 		view.$('#videosettings input').prop('disabled', true);
-		if (this.getVideoType(url) == 'url'){
-                        view.$('#videosrc').val(url);
-		}
+		if(this.getVideoType(url) == 'url') view.$('#videosrc').val(url);
         }
-
     },
-    buildYouTubeLink: function(id, startmin, startsec, endmin, endsec, autoplay){
-
-	var url =  'http://www.youtube.com/embed/'+id, start = 0, end = 0;
+    buildYouTubeLink: function(id, startmin, startsec, endmin, endsec, autoplay) {
+	var url = 'http://www.youtube.com/embed/'+id, start = 0, end = 0;
 	if(startmin != '') start += parseInt(startmin)*60;
 	if(startsec != '') start += parseInt(startsec);
 	if(endmin != '') end += parseInt(endmin)*60;
 	if(endsec != '') end  += parseInt(endsec);
-	if (start != 0){
+	if(start != 0) {
 		url += '?start='+start;
-		if ((end != 0)&&(start < end)) url += '&end='+end;
-	}else{
-		if (end != 0) url += '?end='+end;
+		if((end != 0) && (start < end)) url += '&end=' + end;
 	}
+	else if (end != 0) url += '?end='+end;
 	if(autoplay) {
-		if((start != 0)||(end != 0))
-		url += '&autoplay=1';
+		if((start != 0)||(end != 0)) url += '&autoplay=1';
 		else url += '?autoplay=1';
 	}
 	return url;
     },
-    buildMatterhornLink: function(url, startmin, startsec, autoplay, controls){
+    buildMatterhornLink: function(url, startmin, startsec, autoplay, controls) {
 	var start = '';
 	if(startmin != '') start += startmin + 'm';
         if(startsec != '') start += startsec + 's';
-        if (start != '') url += '&t='+start;
-	if (autoplay)  url += '&play=true'; 
-	if (controls) url += '&hideControls=true'; else url +='&hideControls=false';
+        if(start != '') url += '&t='+start;
+	if(autoplay) url += '&play=true'; 
+	if(controls) url += '&hideControls=true'; 
+	else url += '&hideControls=false';
         return url;
-
     },
-    showPreview: function(view, url){
+    showPreview: function(view, url) {
 	var iframe = view.$('iframe');
 	iframe.attr('src', url);
     }
--- blocks/VideoBlock/js/author_view.js
+++ blocks/VideoBlock/js/author_view.js
@@ -9,62 +9,43 @@ define(['assets/js/author_view', 'assets/js/url', 'utils'], function (
 	    "click #videotype option": "selection",
 	    "click button[name=preview]": "preview"
         },
-        initialize: function (options) {
+        initialize: function(options) {
 	},
-        render: function() { return this; },
-        postRender: function () {
+        render: function() {
+		return this; 
+	},
+        postRender: function() {
             Utils.showPreview(this, this.$('#videourl').text());
-            var
-                view = this,
-                url = view.$('#videourl').text(),
-                videotype = Utils.getVideoType(url);
-
-            if(url=='') {
-                view.$('iframe').hide();
-            }
-
-            view.$("#videotype option[value="+videotype+"]").attr('selected', true);
-            view.selection();
+            var url = this.$('#videourl').text(), videotype = Utils.getVideoType(url);
+            if(url == '') this.$('iframe').hide();
+            this.$("#videotype option[value="+videotype+"]").attr('selected', true);
+            this.selection();
         },
-
-	selection: function(){
+	selection: function() {
 		var videotype = this.$('#videotype').val();
-		if (videotype == 'youtube'){
-			this.$('#videosrcname').html('YouTube ID');
-		}else {
-			this.$('#videosrcname').html('URL');
-		}
+		if (videotype == 'youtube') this.$('#videosrcname').html('YouTube ID');
+		else this.$('#videosrcname').html('URL');
 		Utils.resetVideoData(this);
 		Utils.setVideoData(this, this.$('#videourl').text(), videotype);
 	},
-	preview: function(){
-		var
-		view = this, 
-		videourl = view.$('#videourl'),
-		videotype = view.$('#videotype').val(),
-		url = Utils.getUrl(view, videotype);
-		if (url != ''){
-     			view.$('iframe').show();
+	preview: function() {
+		var videourl = this.$('#videourl'), videotype = this.$('#videotype').val(), url = Utils.getUrl(this, videotype);
+		if(url != '') {
+     			this.$('iframe').show();
 			videourl.html(url);
-			Utils.showPreview(view, url);
-		}else this.selection();
+			Utils.showPreview(this, url);
+		}
+		else this.selection();
 	},
-
-        saveVideo: function () {
+        saveVideo: function() {
             this.preview();
-	    var view = this;
-            var url = view.$('#videourl').text();
-            var status = view.$('.status');
-	    
-            status.text('Speichere Änderungen...');
-            helper
-                .callHandler(view.model.id, 'save', { url: url })
-                .then(function () {
-                    status.text('Änderungen wurden gespeichert.');
-                    view.switchBack();
-                }, function (error) {
-                    status.text('Fehler beim Speichern: '+jQuery.parseJSON(error.responseText).reason);
-                });
+	    var url = this.$('#videourl').text(), status = this.$('.status');
+	    status.text('Speichere Änderungen...');
+            helper.callHandler(this.model.id, 'save', { url: url }).then(function() {
+            	status.text('Änderungen wurden gespeichert.');
+            	this.switchBack();
+            	}, function (error) {status.text('Fehler beim Speichern: '+jQuery.parseJSON(error.responseText).reason);
+	    });
         }
     });
 });
