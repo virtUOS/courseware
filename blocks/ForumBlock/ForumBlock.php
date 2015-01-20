f3991de150cdc223fc9d6a90ed510e2de7bb3eb8
luniki <luniki@gmail.com>
2015-01-20 14:32:10 +0100

    refactor forum block

--- blocks/ForumBlock/ForumBlock.php
+++ blocks/ForumBlock/ForumBlock.php
@@ -6,9 +6,9 @@ use Mooc\UI\Block;
 use Mooc\UI\Section\Section;
 
 /**
- * Display the contents of a Blubber stream in a (M)ooc.IP block.
+ * Display the contents of an area of the core forum.
  *
- * @author Christian Flothmann <christian.flothmann@uos.de>
+ * @author <tgloeggl@uos.de>
  */
 class ForumBlock extends Block
 {
@@ -17,44 +17,7 @@ class ForumBlock extends Block
     public function initialize()
     {
         $this->defineField('area_id', \Mooc\SCOPE_BLOCK, -1);
-
-        $seminar_id = $this->container['cid'];
-        $user_id    = $this->container['current_user']->id;
-
-        // predefined category must have the id md5('mooc' . seminar_id)
-        $category_id = md5('mooc' . $seminar_id);
-
-        // check, that correct categories and areas present
-        if (!\ForumCat::get($category_id)) {
-            $stmt = \DBManager::get()->prepare("INSERT INTO forum_categories
-                (category_id, seminar_id, entry_name)
-                VALUES (?, ?, ?)");
-
-            $stmt->execute(array($category_id, $seminar_id, 'Diskussionen zu den Aufgaben'));
-        }
-
-        if ($this->area_id == -1 || !\ForumEntry::getConstraints($this->area_id)) {
-            $this->area_id = md5(uniqid());
-
-            $parent = $this->getModel();
-            while ($parent = $parent->parent) {
-                if ($parent->type == 'Courseware') {
-                    break;
-                }
-                $path[] = $parent->title;
-            }
-
-            \ForumEntry::insert(array(
-                'topic_id'    => $this->area_id,
-                'seminar_id'  => $seminar_id,
-                'user_id'     => $user_id,
-                'name'        => implode(' > ', array_reverse($path)),
-                'author'      => get_fullname(),
-                'author_host' => getenv('REMOTE_ADDR')
-            ), $seminar_id);
-
-            \ForumCat::addArea($category_id, $this->area_id);
-        }
+        $this->connectForum();
     }
 
     public function student_view()
@@ -88,5 +51,66 @@ class ForumBlock extends Block
     public function isEditable()
     {
         return false;
+    }
+
+    // connect this block with an area of the forum
+    private function connectForum()
+    {
+        $seminar_id = $this->container['cid'];
+        $user_id    = $this->container['current_user']->id;
+
+        $category_id = $this->findOrCreateCategory();
+
+        if ($this->area_id != -1 && \ForumEntry::getConstraints($this->area_id)) {
+            return;
+        }
+
+        $this->area_id = md5(uniqid());
+
+        \ForumEntry::insert(
+            array(
+                'topic_id'    => $this->area_id,
+                'seminar_id'  => $seminar_id,
+                'user_id'     => $user_id,
+                'name'        => $this->getParentTitles(),
+                'author'      => get_fullname(),
+                'author_host' => getenv('REMOTE_ADDR')
+            ), $seminar_id);
+
+        \ForumCat::addArea($category_id, $this->area_id);
+    }
+
+    private function findOrCreateCategory()
+    {
+        $seminar_id = $this->container['cid'];
+
+        // predefined category must have the id md5('mooc' . seminar_id)
+        $category_id = md5('mooc' . $seminar_id);
+
+        // check, that correct categories and areas present
+        if (!\ForumCat::get($category_id)) {
+            $stmt = \DBManager::get()->prepare("INSERT INTO forum_categories
+                (category_id, seminar_id, entry_name)
+                VALUES (?, ?, ?)");
+
+            $stmt->execute(array($category_id, $seminar_id, 'Diskussionen zu den Aufgaben'));
+        }
+
+        return $category_id;
+    }
+
+    // get parents' titles
+    private function getParentTitles()
+    {
+        $path = array();
+        $parent = $this->getModel();
+
+        while ($parent = $parent->parent) {
+            if ($parent->type == 'Courseware') {
+                break;
+            }
+            $path[] = $parent->title;
+        }
+        return implode(' > ', array_reverse($path));
     }
 }
