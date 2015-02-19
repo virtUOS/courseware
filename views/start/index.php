24fc336371010a3bf9fa5a4a753337a33dafb3cd
luniki <luniki@gmail.com>
2015-02-19 14:07:41 +0100

    fix #344

--- views/start/index.php
+++ views/start/index.php
@@ -1,30 +1,35 @@
-<section id="mooc-course-list">
-<? foreach ($courses as $course) : ?>
-    <article>
-        <div class="course-avatar-wrapper">
-            <img class="course-avatar-medium course-<?= $course->id ?>"
-                 alt="<?= $name = htmlReady($course->name) ?>"
-                 title="<?= $name ?>"
-                 src="<?= $preview_images[$course->id] ?: CourseAvatar::getAvatar($course->id)->getURL(CourseAvatar::MEDIUM) ?>" />
-        </div>
+<? if (sizeof($courses)) : ?>
+  <section id="mooc-course-list">
+  <? foreach ($courses as $course) : ?>
+      <article>
+          <div class="course-avatar-wrapper">
+              <img class="course-avatar-medium course-<?= $course->id ?>"
+                   alt="<?= $name = htmlReady($course->name) ?>"
+                   title="<?= $name ?>"
+                   src="<?= $preview_images[$course->id] ?: CourseAvatar::getAvatar($course->id)->getURL(CourseAvatar::MEDIUM) ?>" />
+          </div>
 
 
-        <h1><?= $name ?></h1>
-        <p class=subtitle><?= htmlReady($course->untertitel) ?></p>
+          <h1><?= $name ?></h1>
+          <p class=subtitle><?= htmlReady($course->untertitel) ?></p>
 
-        <?= \Studip\LinkButton::create(_('Kurs anzeigen'),
-                                       PluginEngine::getLink($plugin,
-                                                             array('cid' => $course->id),
-                                                             'courses/show/'.$course->id,
-                                                             true)) ?>
+          <?= \Studip\LinkButton::create(_('Kurs anzeigen'),
+                                         PluginEngine::getLink($plugin,
+                                                               array('cid' => $course->id),
+                                                               'courses/show/'.$course->id,
+                                                               true)) ?>
 
-        <? $suppose_to_kill_link = \URLHelper::getLink(
-            "dispatch.php/my_courses/decline/$course->id",
-            array(),
-            true); ?>
+          <? $suppose_to_kill_link = \URLHelper::getLink(
+              "dispatch.php/my_courses/decline/$course->id",
+              array(),
+              true); ?>
 
-        <a class="kill" href="<?= $suppose_to_kill_link  ?>"><?= _("Mitgliedschaft beenden") ?></a>
-    </article>
+          <a class="kill" href="<?= $suppose_to_kill_link  ?>"><?= _("Mitgliedschaft beenden") ?></a>
+      </article>
 
-<? endforeach ?>
-</section>
+  <? endforeach ?>
+  </section>
+<? else : ?>
+  <p><?= _("Sie sind noch in keinem Mooc-Kurs eingetragen. ") ?></p>
+  <?= \Studip\LinkButton::create('Zur Kursliste', PluginEngine::getURL($plugin, array(), 'courses/index')) ?>
+<? endif ?>
