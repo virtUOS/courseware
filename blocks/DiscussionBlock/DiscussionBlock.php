b145ad496f7b8c456354b0bb1f3fd82c341f0efb
luniki <luniki@gmail.com>
2015-05-29 09:33:39 +0200

    add DiscussionBlock's scaffolding, refs dfb#279

---
+++ blocks/DiscussionBlock/DiscussionBlock.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Mooc\UI\DiscussionBlock;
+
+use Mooc\UI\Block;
+
+/**
+ */
+class DiscussionBlock extends Block
+{
+    const NAME = 'Diskussion';
+
+    function initialize()
+    {
+    }
+
+    function student_view()
+    {
+        return array();
+    }
+
+    function author_view()
+    {
+        return array();
+    }
+}
---
+++ blocks/DiscussionBlock/js/student_view.js
@@ -0,0 +1,15 @@
+define(['assets/js/student_view', 'assets/js/url', 'assets/js/templates'], function (StudentView, helper, templates) {
+
+    'use strict';
+
+    return StudentView.extend({
+        events: {},
+
+        initialize: function (options) {
+        },
+
+        render: function() {
+            return this;
+        }
+    });
+});
