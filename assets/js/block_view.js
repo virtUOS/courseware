7c0176dcffe00a1506fb4bfcf786df1705766765
luniki <luniki@gmail.com>
2015-06-18 09:09:51 +0200

    add initializeFromDOM to JS BlockViews, refs #8

--- assets/js/block_view.js
+++ assets/js/block_view.js
@@ -4,6 +4,9 @@ define(['backbone', 'assets/js/url'], function (Backbone, helper) {
         // filled by block type's createView method
         block_type: null,
 
+        initializeFromDOM: function () {
+        },
+
         renderServerSide: function () {
             var self = this;
 
@@ -11,6 +14,11 @@ define(['backbone', 'assets/js/url'], function (Backbone, helper) {
                 .then(
                     function (data) {
                         self.$el.html(data);
+
+                        // let the block initialize from the just
+                        // inserted DOM
+                        self.initializeFromDOM();
+
                         if (typeof self.postRender === "function") {
                             self.postRender();
                         }
