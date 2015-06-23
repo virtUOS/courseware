111cdabd599aebf80148d8d78556bf87558807e1
André <andre@gundk.it>
2015-06-23 14:35:15 +0200

    add new Evaluationsblock to courseware, fixes #12

---
+++ blocks/EvaluationBlock/js/student_view.js
@@ -0,0 +1,11 @@
+define(['assets/js/student_view'],
+       function (StudentView) {
+
+    'use strict';
+
+    return StudentView.extend({
+        render: function() {
+            return this;
+        }
+    });
+});
