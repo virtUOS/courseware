81bb4afc061fc8371cda0e80bf216a68a7d7fad7
André <andre@gundk.it>
2015-06-23 11:51:01 +0200

    appy changes for new Evaluationsblock to courseware

---
+++ blocks/EvaluationBlock/js/student_view.js
@@ -0,0 +1,21 @@
+define(['assets/js/student_view'],
+       function (StudentView) {
+
+    'use strict';
+
+    return StudentView.extend({
+        events: {
+        },
+
+        initialize: function() {
+        },
+
+        render: function() {
+            return this;
+        },
+
+        postRender: function () {
+
+        }
+    });
+});
