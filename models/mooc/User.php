virtUOS/courseware

commit 6ffb67880bfcfcc4e62ad251d2849780dce50401

add adjustable edit perm for courseware, refs #26

models/mooc/User.php
@@ -36,7 +36,7 @@ class User extends \User
         }
 
         if ($model instanceof DbBlock) {
-            return $this->hasPerm($model->seminar_id, 'dozent');
+            return $this->canEditBlock($model);
         }
 
         throw new \RuntimeException('not implemented: ' . __METHOD__);
@@ -66,7 +66,7 @@ class User extends \User
         }
 
         if ($model instanceof DbBlock) {
-            return $this->hasPerm($model->seminar_id, 'tutor');
+            return $this->canEditBlock($model);
         }
 
         throw new \RuntimeException('not implemented: ' . __METHOD__);
@@ -79,7 +79,7 @@ class User extends \User
         }
 
         if ($model instanceof DbBlock) {
-            return $this->hasPerm($model->seminar_id, 'dozent');
+            return $this->canEditBlock($model);
         }
 
         throw new \RuntimeException('not implemented: ' . __METHOD__);
@@ -91,14 +91,34 @@ class User extends \User
             return false;
         }
 
-        /** @var \Seminar_Perm $perm */
-        $perm = $GLOBALS['perm'];
-
-        return $perm->have_studip_perm($perm_level, $cid, $this->id);
+        return $GLOBALS['perm']->have_studip_perm($perm_level, $cid, $this->id);
     }
 
-    public function getPerm()
+    public function getPerm($cid)
     {
-        return $this->perms;
+        if (!$cid) {
+            return false;
+        }
+
+        return $GLOBALS['perm']->get_studip_perm($cid, $this->id);
+    }
+
+    /////////////////////
+    // PRIVATE HELPERS //
+    /////////////////////
+
+    // get the editing permission level from the courseware's settings
+    private function canEditBlock($block)
+    {
+        // optimistically get the current courseware
+        $courseware = $this->container['current_courseware'];
+
+        // if the $block is not a descendant of it, get its courseware
+        if ($courseware->seminar_id !== $block->seminar_id) {
+            $courseware_model = $this->container['courseware_factory']->makeCourseware($block->seminar_id);
+            $courseware = $this->container['block_factory']->makeBlock($courseware_model);
+        }
+
+        return $this->hasPerm($block->seminar_id, $courseware->getEditingPermission());
     }
 }

blocks/Courseware/Courseware.php
@@ -14,11 +14,21 @@ class Courseware extends Block {
     const PROGRESSION_FREE = 'free';
     const PROGRESSION_SEQ  = 'seq';
 
+    // 'tutor' and 'dozent'  may edit courseware
+    const EDITING_PERMISSION_TUTOR  = 'tutor';
+
+    // only 'dozent'  may edit courseware
+    const EDITING_PERMISSION_DOZENT = 'dozent';
 
     function initialize()
     {
         $this->defineField('lastSelected', \Mooc\SCOPE_USER, null);
-        $this->defineField('progression',  \Mooc\SCOPE_BLOCK, self::PROGRESSION_FREE);
+
+        // FIXME: this must be stored somewhere else, see https://github.com/virtUOS/courseware/issues/16
+        $this->defineField('progression', \Mooc\SCOPE_BLOCK, self::PROGRESSION_FREE);
+
+        // FIXME: this must be stored somewhere else, see https://github.com/virtUOS/courseware/issues/16
+        $this->defineField('editing_permission', \Mooc\SCOPE_BLOCK, self::EDITING_PERMISSION_TUTOR);
     }
 
     function student_view($context = array())
@@ -219,6 +229,29 @@ class Courseware extends Block {
 
         return null;
     }
+
+    // FIXME: this must be stored somewhere else, see https://github.com/virtUOS/courseware/issues/16
+    // set perm level of editing permission
+    public function setEditingPermission($perm_level)
+    {
+        if (in_array($perm_level, array(self::EDITING_PERMISSION_TUTOR, self::EDITING_PERMISSION_DOZENT))) {
+            $this->editing_permission = $perm_level;
+            return true;
+        }
+
+        return false;
+    }
+
+    // FIXME: this must be stored somewhere else, see https://github.com/virtUOS/courseware/issues/16
+    // get perm level of editing permission
+    public function getEditingPermission()
+    {
+        return $this->editing_permission;
+    }
+
+    ///////////////////////
+    // PRIVATE FUNCTIONS //
+    ///////////////////////
 
     private function getSelected($context)
     {

controllers/courseware.php
@@ -1,5 +1,7 @@
 <?php
 
+use Mooc\UI\Courseware\Courseware;
+
 class CoursewareController extends MoocipController {
 
     public function before_filter(&$action, &$args)
@@ -30,7 +32,7 @@ class CoursewareController extends MoocipController {
     }
 
 
-    // show this course's settings page
+    // show this course's settings page but only to tutors+
     public function settings_action()
     {
         // only tutor+ may visit this page
@@ -42,6 +44,13 @@ class CoursewareController extends MoocipController {
             Navigation::activateItem('/course/mooc_courseware/settings');
         }
 
+        $user = $this->container['current_user'];
+
+        if (!$user->hasPerm($this->container['cid'], 'tutor')) {
+            throw new Trails_Exception(401);
+        }
+
+        $this->is_tutor = $user->getPerm($this->container['cid']) === 'tutor';
 
         if (Request::isPost()) {
             CSRFProtection::verifyUnsafeRequest();
@@ -115,9 +124,17 @@ class CoursewareController extends MoocipController {
             $this->storeCoursewareTitle($courseware_settings['title']);
         }
 
+        ////////////////////////////
+        // COURSEWARE PROGRESSION //
+        ////////////////////////////
         if (isset($courseware_settings['progression'])) {
             $this->storeCoursewareProgressionType($courseware_settings['progression']);
         }
+
+        ////////////////////////
+        // EDITING PERMISSION //
+        ////////////////////////
+        $this->storeEditingPermission(isset($courseware_settings['editing_permission']) ? true : false);
 
         $this->courseware_block->save();
     }
@@ -136,6 +153,31 @@ class CoursewareController extends MoocipController {
     private function storeCoursewareProgressionType($type)
     {
         if (!$this->courseware_block->setProgressionType($type)) {
+            // TODO: send a message back
+        }
+    }
+
+    private function storeDiscussionBlockActivation($active)
+    {
+        if (!$this->courseware_block->setDiscussionBlockActivation($active)) {
+            // TODO: send a message back
+        }
+    }
+
+    private function storeEditingPermission($tutor_may_edit)
+    {
+        $perm = $tutor_may_edit
+              ? Courseware::EDITING_PERMISSION_TUTOR
+              : Courseware::EDITING_PERMISSION_DOZENT;
+
+        // tutors may not edit the courseware, thus they may not edit
+        // this setting
+        if ($perm === Courseware::EDITING_PERMISSION_DOZENT &&
+            $this->container['current_user']->getPerm($this->container['cid']) === 'tutor') {
+            throw new Trails_Exception(401, _("Tutoren können diese Einstellung nicht speichern."));
+        }
+
+        if (!$this->courseware_block->setEditingPermission($perm)) {
             // TODO: send a message back
         }
     }

views/courseware/_settings_editing_permission.php
@@ -1,0 +1,16 @@
+<tr>
+    <td>
+        <label for="courseware-editing-permission">
+            <?= _('Editierberechtigung für Tutoren') ?><br>
+            <dfn id="courseware-editing-permission-description">
+                <?= _('Wenn Sie diesen Schalter aktivieren, dürfen neben den Dozenten auch Tutoren Ihre Courseware editieren.'); ?>
+            </dfn>
+        </label>
+    </td>
+    <td>
+        <input id="courseware-editing-permission"
+               name="courseware[editing_permission]"
+               <? if ($is_tutor) : ?> disabled <? endif ?>
+               type="checkbox" <?= $courseware_block->getEditingPermission() === \Mooc\UI\Courseware\Courseware::EDITING_PERMISSION_TUTOR ? "checked" : "" ?>>
+    </td>
+</tr>

views/courseware/settings.php
@@ -54,6 +54,9 @@ use Mooc\UI\Courseware\Courseware;
                     </select>
                 </td>
             </tr>
+
+            <?= $this->render_partial('courseware/_settings_editing_permission') ?>
+
         </tbody>
 
         <tfoot>

commit 7ffc051f368ca773709c750118af0edeb732cfdf

fix fatal error introduced in 6ffb678

models/mooc/User.php
@@ -108,13 +108,13 @@ class User extends \User
     /////////////////////
 
     // get the editing permission level from the courseware's settings
-    private function canEditBlock($block)
+    private function canEditBlock(DbBlock $block)
     {
         // optimistically get the current courseware
         $courseware = $this->container['current_courseware'];
 
         // if the $block is not a descendant of it, get its courseware
-        if ($courseware->seminar_id !== $block->seminar_id) {
+        if ($courseware->getModel()->seminar_id !== $block->seminar_id) {
             $courseware_model = $this->container['courseware_factory']->makeCourseware($block->seminar_id);
             $courseware = $this->container['block_factory']->makeBlock($courseware_model);
         }