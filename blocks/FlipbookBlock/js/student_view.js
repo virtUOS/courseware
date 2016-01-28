2fe4819e80a89876350a17644da967fff89573b2
rlucke <rlucke@uos.de>
2016-01-28 10:06:44 +0100

    add fullscreen view for flipbook block

--- blocks/FlipbookBlock/js/student_view.js
+++ blocks/FlipbookBlock/js/student_view.js
@@ -19,7 +19,7 @@ define(['assets/js/student_view', 'assets/js/vendor/turn/turn'],
         },
 
         postRender: function () {
-            $('#flipbook').turn({
+            $('.flipbook').turn({
                 display: 'double',
                 acceleration: true,
                 gradients: !$.isTouch,
@@ -42,46 +42,73 @@ define(['assets/js/student_view', 'assets/js/vendor/turn/turn'],
             });
             
             $(".flipbook-left-control").click(function(){
-                    $('#flipbook').turn('previous');
+                    $('.flipbook').turn('previous');
             });
             
             $(".flipbook-right-control").click(function(){
-                    $('#flipbook').turn('next');
+                    $('.flipbook').turn('next');
             });
             
-            $('#flipbook').bind('DOMMouseScroll', function(e){
+            $('.flipbook').bind('DOMMouseScroll', function(e){
                  if(e.originalEvent.detail > 0) {
                      //scroll down
-                     $('#flipbook').turn('next');
+                     $('.flipbook').turn('next');
                  } else {
                      //scroll up
-                     $('#flipbook').turn('previous');
+                     $('.flipbook').turn('previous');
                  }
                  //prevent page fom scrolling
                  return false;
             });
             
-            $('#flipbook').bind('mousewheel', function(e){
+            $('.flipbook').bind('mousewheel', function(e){
                 if(e.originalEvent.wheelDelta < 0) {
                      //scroll down
-                     $('#flipbook').turn('next');
+                     $('.flipbook').turn('next');
                  } else {
                      //scroll up
-                     $('#flipbook').turn('previous');
+                     $('.flipbook').turn('previous');
                  }
 
-                 //prevent page fom scrolling
+                 //prevent page from scrolling
                 return false;
             });
             
             $(window).bind('keydown', function(e){
     
                 if (e.keyCode==37)
-                    $('#flipbook').turn('previous');
+                    $('.flipbook').turn('previous');
                 else if (e.keyCode==39)
-                    $('#flipbook').turn('next');
+                    $('.flipbook').turn('next');
                     
             });
+            $('.flipbookfullon').on('click', function(e){
+                var $height = $(window).height();
+                var $width = $(window).width();
+                console.log("height: "+$height+" width: "+$width);
+                $(".flipbookoverlay").height($height).show();
+                $('.flipbookimgfull').height($height*0.95);
+                $('.flipbookimgfull-container').height($height*0.95).width(($('.flipbookimgfull').width()*2)+5);
+                
+                var $currentimage = $(".flipbook").turn('view');
+                $.each($currentimage, function($i, $v){
+                    if ($v == 0) { $('.flipbookimgfull-container').width($('.flipbookimgfull').width()); return;}
+                    var $flipbookimage = $(".p"+$v).attr("image-data");
+                    var $bigpicture = $('.flipbookimgfull[image-data="'+$flipbookimage+'"]');
+                    console.log($bigpicture);
+                    $bigpicture.show();
+                    
+                });
+                $('body').css("overflow", "hidden");
+            });
+            
+            $(".flipbookfulloff").on("click", function(e){
+                 $(".flipbookoverlay").hide();
+                 $(".flipbookimgfull").hide();
+                 $('body').css("overflow", "auto");
+            });
+        
+            
             
 
         }
