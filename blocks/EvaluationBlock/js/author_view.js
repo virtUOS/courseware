00beae682f35bdd76a657dda22d0eff4dab2ff8f
Nils Oesting <noesting@uni-osnabrueck.de>
2016-04-01 14:37:39 +0200

    Add author_view to evaluation block

---
+++ blocks/EvaluationBlock/js/author_view.js
@@ -0,0 +1,12 @@
+define(['assets/js/author_view'], function (AuthorView) {
+    'use strict';
+
+    return AuthorView.extend({
+        initialize: function (options) {
+        },
+
+        render: function () {
+            return this;
+        }
+    });
+});
