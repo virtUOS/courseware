3d43c146eedd95d48218bed227b5b4f2252102ac
luniki <luniki@gmail.com>
2016-04-12 13:42:59 +0200

    fix namespace

--- Courseware.php
+++ Courseware.php
@@ -5,7 +5,7 @@ require_once __DIR__.'/vendor/autoload.php';
 use Courseware\Container;
 use Mooc\DB\CoursewareFactory;
 use Mooc\UI\BlockFactory;
-use Mooc\User;
+use Courseware\User;
 
 /**
  * MoocIP.class.php
