300d13369b7236c450d49bfde5edf3d1697ebe01
luniki <luniki@gmail.com>
2016-04-26 10:33:09 +0200

    fix the fix

--- blocks/Courseware/js/student_view.js
+++ blocks/Courseware/js/student_view.js
@@ -115,6 +115,7 @@ define(['backbone', 'assets/js/url', 'assets/js/block_model', 'assets/js/student
             }
 
             tooltip(this.$el, 'button');
+            this.resizeColumnHeights();
         },
 
         navigateTo: function (event) {
