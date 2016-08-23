948050c1c9a6c2a4c4ee2392097ec494838e75e2
Ron Lucke <rlucke@uni-osnabrueck.de>
2016-08-23 11:03:07 +0200

    add DownloadBlock

---
+++ blocks/DownloadBlock/DownloadBlock.php
@@ -0,0 +1,165 @@
+<?
+namespace Mooc\UI\DownloadBlock;
+
+use Mooc\UI\Block;
+
+class DownloadBlock extends Block 
+{
+    const NAME = 'Download';
+
+    function initialize()
+    {
+        $this->defineField('file', \Mooc\SCOPE_BLOCK, '');
+        $this->defineField('file_id', \Mooc\SCOPE_BLOCK, '');
+        $this->defineField('file_name', \Mooc\SCOPE_BLOCK, '');
+        $this->defineField('folder_id', \Mooc\SCOPE_BLOCK, '');
+        $this->defineField('folder_tmp', \Mooc\SCOPE_BLOCK, '');
+    }
+
+    function student_view()
+    {
+        
+        return array_merge($this->getAttrArray(), ['confirmed' => !! $this->getProgress()->grade]);
+    }
+
+    function author_view()
+    {
+        $this->authorizeUpdate();
+        $this->setFolderId();
+        $allfiles = $this->showFiles($this->folder_id);
+        return array_merge($this->getAttrArray(), ["allfiles" => $allfiles]);
+    }
+
+    public function save_handler(array $data)
+    {
+        $this->authorizeUpdate();
+        
+        if (isset ($data['file'])) {
+            $this->file = (string) $data['file'];
+            $this->file_id = (string) $data['file_id'];
+            $this->file_name = (string) $data['file_name'];
+             
+        } else {
+            $this->file_id = "";
+            $this->file_name = "";
+        }
+        
+        return;
+    }
+    
+    function download_handler($data)
+    {
+        $this->setGrade(1.0);
+        return ;
+    }
+    
+    private function setFolderId($foldername = "Allgemeiner Dateiordner"){
+        $cid = $this->container['cid'];
+        $db = \DBManager::get();
+        $stmt = $db->prepare("SELECT folder_id FROM folder WHERE name = :foldername AND seminar_id = :cid");
+        $stmt->bindParam(":foldername", $foldername);
+        $stmt->bindParam(":cid", $cid);
+        $stmt->execute();
+        $this->folder_id = $stmt->fetch()['folder_id'];
+        return ;
+    }
+    
+    private function showFiles($folderId, $filetype = "")
+    {
+        $db = \DBManager::get();
+        $stmt = $db->prepare("SELECT * FROM `dokumente` WHERE `range_id` = :range_id
+            ORDER BY `name`");
+        $stmt->bindParam(":range_id", $folderId);
+        $stmt->execute();
+        $response = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        $filesarray = array();
+        foreach ($response as $item) {
+            if($this->file !=  $item['name']){
+                $filesarray[] = $item;
+            }
+        }
+        return $filesarray;
+    }
+
+    private function getAttrArray() 
+    {
+        return array(
+            'file' => $this->file, 
+            'file_id' => $this->file_id, 
+            'file_name' => $this->file_name, 
+            'folder_id' => $this->folder_id
+            
+        );
+    }
+
+    public function exportProperties()
+    {
+       return array(
+            'file' => $this->file,
+            'file_id' => $this->file_id,
+            'file_name' => $this->file_name,
+       );
+    }
+    
+    public function getFiles()
+    {
+        $document = new \StudipDocument($this->file_id);
+        $files[] = array (
+            'id' => $this->file_id,
+            'name' => $this->file_name,
+            'description' => $document->description,
+            'filename' => $document->filename,
+            'filesize' => $document->filesize,
+            'url' => $document->url,
+            'path' => get_upload_file_path($this->file_id),
+        );
+        return $files;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getXmlNamespace()
+    {
+        return 'http://moocip.de/schema/block/download/';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getXmlSchemaLocation()
+    {
+        return 'http://moocip.de/schema/block/download/download-1.0.xsd';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function importProperties(array $properties)
+    {
+        if (isset($properties['file'])) {
+            $this->download = $properties['file'];
+        }
+        if (isset($properties['file_id'])) {
+            $this->download = $properties['file_id'];
+        }
+        if (isset($properties['file_name'])) {
+            $this->download = $properties['file_name'];
+        }
+        
+        $this->setFolderId();
+
+        $this->save();
+    }
+    
+    public function importContents($contents, array $files)
+    {
+        $file = reset($files);
+        $this->file = $file->name;
+        $document =  current(\StudipDocument::findBySQL('filename = ?', array($this->file)));
+        $this->file_id = $document->dokument_id;
+
+        $this->save();
+    }
+    
+}
---
+++ blocks/DownloadBlock/js/student_view.js
@@ -0,0 +1,42 @@
+define(['assets/js/student_view', 'assets/js/url', 'assets/js/templates'], function (StudentView, helper, templates) {
+    
+    'use strict';
+    
+    return StudentView.extend({
+        events: {
+            "click button[name=download]":   "onDownload",
+        },
+        
+        initialize: function(options) {
+        },
+
+        render: function() {
+            return this; 
+        },
+        
+        onDownload: function() {
+            this.model.set('confirmed', true);
+            this.model.set('file', this.$("input[name='file']").val());
+            this.model.set('file_name', this.$("input[name='file_name']").val());
+            this.model.set('file_id', this.$("input[name='file_id']").val());
+            this.$el.html(templates("DownloadBlock", 'student_view', _.clone(this.model.attributes)));
+            helper
+                .callHandler(this.model.id, 'download', {})
+                .then(
+                    // success
+                    function () {
+                        
+                    },
+
+                    // error
+                    function (error) {
+                        var errorMessage = 'Could not update the block: '+jQuery.parseJSON(error.responseText).reason;
+                        alert(errorMessage);
+                        console.log(errorMessage, arguments);
+                    })
+                .done();
+        }
+    });
+});
+
+
