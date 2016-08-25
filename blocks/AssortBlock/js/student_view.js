0c87ec8582d66eb9c1b663e2b8806b410c744b02
Ron Lucke <rlucke@uni-osnabrueck.de>
2016-08-25 09:20:41 +0200

    add exception handling for AssortBlock

--- blocks/AssortBlock/js/student_view.js
+++ blocks/AssortBlock/js/student_view.js
@@ -9,18 +9,26 @@ define(['assets/js/student_view'], function (StudentView) {
         
         postRender: function() {
             var $assorttype = this.$(".assorttype-selection-assort").val();  
+            if (!$assorttype) {return;}
             var $div = $("<div>", {class: "assortblock-content"});
             this.$el.append($div);
             if($assorttype == "tabs" || $assorttype == "vtabs") {
                 var $ul = $("<ul>");
                 $div.append($ul);
             }
-            var $assortblocks = JSON.parse(this.$(".assortblocks-selection-assort").val());
-            console.log($assortblocks.length);
+            try {
+                var $assortblocks = JSON.parse(this.$(".assortblocks-selection-assort").val());
+            } catch (e) {
+                    console.log("json parse crashed!");
+                    console.log(e);
+                    return;
+            }
             var $maxheight = 0;
+            if(!$assortblocks) {return ;}
             $.each($assortblocks , function(){
                 var $id = this["id"];
                 var $name = this["name"];
+                if((!$id)||(!$name)) {return;}
                 var $thisblock = $("#block-"+$id);
                 if ($thisblock.length == 0) {console.log("block "+ $id +" nicht vorhanden"); return; }
                 $thisblock.hide();
@@ -36,7 +44,6 @@ define(['assets/js/student_view'], function (StudentView) {
                         $div.append("<div id ='tabs-"+$id+"'>"+$thisblock.html()+"</div>");
                         break;
                 }
-                console.log($maxheight+$assortblocks.length*20);
             });
             switch($assorttype){
                 case "accordion":
