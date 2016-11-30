a90713c881b0ff3f4b72116ec23bbefc957c7a30
rlucke <rlucke@uni-osnabrueck.de>
2016-11-30 08:46:32 +0100

    VideoBlock: add onModeSwitch and onNavigate

--- blocks/VideoBlock/js/author_view.js
+++ blocks/VideoBlock/js/author_view.js
@@ -12,7 +12,30 @@ define(['assets/js/author_view', 'assets/js/url'], function (
             "click button[name=addmediatype]": "addmediatype",
         },
         initialize: function(options) {
+            Backbone.on('beforemodeswitch', this.onModeSwitch, this);
+            Backbone.on('beforenavigate', this.onNavigate, this);
         },
+        onNavigate: function(event) {
+            if(!$("section .block-content button[name=save]").length) return;
+            if(event.isUserInputHandled) return;
+                event.isUserInputHandled = true;
+                Backbone.trigger('preventnavigateto', !confirm('Es gibt nicht gespeicherte Änderungen. Möchten Sie die Seite trotzdem verlassen?'));
+        },
+        onModeSwitch: function (toView, event) {
+            if (toView != 'student') {
+                return;
+            }
+            // the user already switched back (i.e. the is not visible)
+            if (!this.$el.is(':visible')) {
+                return;
+            }
+            // another listener already handled the user's feedback
+            if (event.isUserInputHandled) {
+                return;
+            }
+            event.isUserInputHandled = true;
+            Backbone.trigger('preventviewswitch', !confirm('Es gibt nicht gespeicherte Änderungen. Möchten Sie trotzdem fortfahren?'));
+        }, 
         render: function() {
             return this;
         },
