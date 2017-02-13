c4644fd250dda429ae0b3f552229d215e99a3877
rlucke <rlucke@uni-osnabrueck.de>
2017-02-13 11:15:33 +0100

    bugfix: TestBlock - don't set options if nothing is selected

--- blocks/TestBlock/js/author_view.js
+++ blocks/TestBlock/js/author_view.js
@@ -19,10 +19,11 @@ define(['assets/js/author_view', 'assets/js/url'], function (AuthorView, helper)
         postRender: function() {
             var $input = this.$("input[name='test_questions']");
             var $selection = this.$('select[name="test_id"] option:selected');
-            
-            var options = $.parseJSON($selection.attr('select-data')),
+            if(typeof $selection.attr('select-data') != "undefined") {
+                var options = $.parseJSON($selection.attr('select-data')),
                 $count = parseInt(options.count);
-            $input.attr('max', $count);
+                $input.attr('max', $count);
+            }
         }, 
         
         setMaxNum: function() {
