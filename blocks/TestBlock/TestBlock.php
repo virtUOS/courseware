430593e2cf5238a04aed9829a9512373a7697dd5
rlucke <rlucke@uni-osnabrueck.de>
2017-10-04 09:40:40 +0100

    change encoding

--- blocks/TestBlock/TestBlock.php
+++ blocks/TestBlock/TestBlock.php
@@ -21,7 +21,7 @@ class TestBlock extends Block
     {
         return array(
             'selftest' => _cw('Selbsttest'),
-            'practice' => _cw('Übungsblatt'),
+            'practice' => _cw('Ãœbungsblatt'),
         );
     }
 
