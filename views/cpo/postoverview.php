784a40e73966157f5b89feb19e279b8805691f6c
rlucke <rlucke@uni-osnabrueck.de>
2017-11-09 13:03:11 +0100

    style post overview

--- views/cpo/postoverview.php
+++ views/cpo/postoverview.php
@@ -1,34 +1,54 @@
-<ul>
+<ul class="post-overview">
     <? foreach($threads as $thread): ?>
-    <li><?= $thread["thread_title"]?></li>
+    <li class="post-overview-thread">
+        <h3><?= $thread["thread_title"]?></h3>
         <? if (array_key_exists($thread["thread_id"], ($thrads_in_blocks))) :?>
             <? foreach($thrads_in_blocks[$thread["thread_id"]] as $block): ?>
-                <a href="<?= $block['link']?>">
+                <p class="post-overview-block-link"><a href="<?= $block['link']?>">
                     <?= $block['title']?>
                 </a>
-                <br>
+                </p>
             <? endforeach; ?>
         <? else :?>
             <p>Dieser Thread wird in keinem Block genutzt.</p>
         <? endif; ?>
-
-        <div class="cw-postblock-posts">
-            <? $counter = 0;
-                foreach($thread["posts"] as $post){
-                    if (object_get_visit($_SESSION['SessionSeminar'], "courseware") < strtotime($post["mkdate"])) {
-                        $counter++;
-                    }
-                }?>
-            <? switch ($counter) {
-                case 0:
-                    echo "<p>Es gibt keine neuen Beiträge.</p>";
-                    break;
-                case 1:
-                    echo "<p>Es gibt einen neuen Beitrag</p>";
-                    break;
-                default:
-                    echo "<p>Es gibt " . $counter . " neue Beiträge</p>";
+    <div class="clear"></div>
+        <? $counter = 0;
+            foreach($thread["posts"] as $post){
+                if (object_get_visit($_SESSION['SessionSeminar'], "courseware") < strtotime($post["mkdate"])) {
+                    $counter++;
+                }
             }?>
-        </div>
+        <? switch ($counter) {
+            case 0:
+                echo "<p>Es gibt keine neuen Beiträge.</p>";
+                break;
+            case 1:
+                echo "<p><b>Es gibt einen neuen Beitrag</b></p>";
+                break;
+            default:
+                echo "<p><b>Es gibt " . $counter . " neue Beiträge</b></p>";
+        }?>
+        </li>
     <? endforeach;?>
 </ul>
+<style>
+    .post-overview-thread {
+        list-style: none;
+        border: solid thin #ccc;
+        width: 70%;
+        padding: 0.5em 1em;  
+        margin-bottom: 1em;
+    }
+    .post-overview-thread > h3 {
+        
+    }
+    .post-overview-block-link{
+        float: left;
+        padding: 0.5em 0.5em 0.5em 1em;
+        margin-right: 1em;
+        background-image: url("../../../assets/images/icons/blue/arr_1right.svg");
+        background-repeat: no-repeat;
+        background-position-y: center;
+    }
+</style>
