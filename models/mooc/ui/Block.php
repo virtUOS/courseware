f3fbf25f876a4cb4df2481d0f0e827d9baae9590
asudau <sudau@elan-ev.de>
2018-03-19 13:49:17 +0100

    set chdate on block-save

--- models/mooc/ui/Block.php
+++ models/mooc/ui/Block.php
@@ -133,12 +133,12 @@ abstract class Block {
     // TODO
     function __set($name, $value)
     {
-        // `id` darf nicht geändert werden
+        // `id` darf nicht geÃ¤ndert werden
         if ('id' === $name) {
             throw new \InvalidArgumentException("Cannot mutate attribute 'id'.");
         }
 
-        // `title` wird direkt im SORM-Objekt geändert
+        // `title` wird direkt im SORM-Objekt geÃ¤ndert
         if ('title' === $name) {
             $this->_model->title = $value;
             $this->_model->store();
@@ -513,6 +513,9 @@ abstract class Block {
         if (isset($this->_progress)) {
             $this->_progress->store();
         }
+      
+        $this->_model->chdate = time();
+        $this->_model->store();
     }
 
     // enforce current user with 'canUpdate' permission of this block
