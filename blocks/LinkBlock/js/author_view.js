490f48c47123f6dcbdb178385de90c4eedb6d47a
rlucke <rlucke@uni-osnabrueck.de>
2018-05-30 07:52:15 +0100

    fix https links

--- blocks/LinkBlock/js/author_view.js
+++ blocks/LinkBlock/js/author_view.js
@@ -32,7 +32,10 @@ export default AuthorView.extend({
             $view.$('select.cw-link-target option[value="'+$view.$('.cw-link-stored-target').val()+'"]').prop('selected', true);
             }
             if ($view.$(".cw-link-type option:selected").val() == "external") {
-                $view.$('input.cw-link-target').val($view.$('.cw-link-stored-target').val());
+                $view.$('input.cw-link-target').val($view.$('.cw-link-stored-target').val().replace('http://','').replace('https://',''));
+            }
+            if ($view.$('input.cw-link-stored-target').val().indexOf('https://') > -1) {
+                $view.$('select.cw-link-protocol option[value="https://"]').prop('selected', true);
             }
         }
         if($view.$(".cw-link-stored-title").val() != "") {
@@ -49,11 +52,13 @@ export default AuthorView.extend({
         if ($type == "internal") {
             $view.$("select.cw-link-target").show();
             $view.$("input.cw-link-target").hide();
+            $view.$("select.cw-link-protocol").hide();
         }
 
         if ($type == "external") {
             $view.$("select.cw-link-target").hide();
             $view.$("input.cw-link-target").show();
+            $view.$("select.cw-link-protocol").show();
         }
     },
 
@@ -91,7 +96,9 @@ export default AuthorView.extend({
             var $linktarget = $view.$("select.cw-link-target option:selected").val();
         }
         if ($linktype == "external") {
-            var $linktarget = $view.$("input.cw-link-target").val();
+            var $linkprotocol = $view.$("select.cw-link-protocol option:selected").val();
+            var $linktarget = $view.$("input.cw-link-target").val().replace('http://','').replace('https://','');
+            $linktarget = $linkprotocol+$linktarget;
         }
         var $linktitle = $view.$(".cw-link-title").val();
 
