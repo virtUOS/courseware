3e7827ca7aef2d2f77b58b7f756a85dcd9fb447f
rlucke <rlucke@uni-osnabrueck.de>
2018-06-21 12:03:43 +0100

    LinkBlock: automatic link correction

--- blocks/LinkBlock/js/author_view.js
+++ blocks/LinkBlock/js/author_view.js
@@ -6,9 +6,10 @@ import helper from 'js/url'
 export default AuthorView.extend({
 
     events: {
-        'click button[name=save]':   'onSave',
-        'click button[name=cancel]': 'switchBack',
-        'change select.cw-link-type':'onSelectType'
+        'click button[name=save]' : 'onSave',
+        'click button[name=cancel]' : 'switchBack',
+        'change select.cw-link-type' :'onSelectType',
+        'change input.cw-link-target' : 'onChangeLinkTarget'
     },
 
     initialize() {
@@ -22,23 +23,22 @@ export default AuthorView.extend({
 
     postRender() {
         var $view = this;
-        if($view.$(".cw-link-stored-type").val() != "") {
+        if($view.$('.cw-link-stored-type').val() != '') {
             $view.$('select.cw-link-type option[value="'+$view.$('.cw-link-stored-type').val()+'"]').prop('selected', true);
         }
         this.onSelectType();
-        
-        if($view.$(".cw-link-stored-target").val() != "") {
-            if ($view.$(".cw-link-type option:selected").val() == "internal"){
+        if($view.$('.cw-link-stored-target').val() != '') {
+            if ($view.$('.cw-link-type option:selected').val() == 'internal'){
             $view.$('select.cw-link-target option[value="'+$view.$('.cw-link-stored-target').val()+'"]').prop('selected', true);
             }
-            if ($view.$(".cw-link-type option:selected").val() == "external") {
+            if ($view.$('.cw-link-type option:selected').val() == 'external') {
                 $view.$('input.cw-link-target').val($view.$('.cw-link-stored-target').val().replace('http://','').replace('https://',''));
             }
             if ($view.$('input.cw-link-stored-target').val().indexOf('https://') > -1) {
                 $view.$('select.cw-link-protocol option[value="https://"]').prop('selected', true);
             }
         }
-        if($view.$(".cw-link-stored-title").val() != "") {
+        if($view.$('.cw-link-stored-title').val() != '') {
             $view.$('input.cw-link-title').val($view.$('.cw-link-stored-title').val());
         }
 
@@ -47,18 +47,18 @@ export default AuthorView.extend({
 
     onSelectType(){
         var $view = this;
-        var $type = $view.$(".cw-link-type option:selected").val();
+        var $type = $view.$('.cw-link-type option:selected').val();
 
-        if ($type == "internal") {
-            $view.$("select.cw-link-target").show();
-            $view.$("input.cw-link-target").hide();
-            $view.$("select.cw-link-protocol").hide();
+        if ($type == 'internal') {
+            $view.$('select.cw-link-target').show();
+            $view.$('input.cw-link-target').hide();
+            $view.$('select.cw-link-protocol').hide();
         }
 
-        if ($type == "external") {
-            $view.$("select.cw-link-target").hide();
-            $view.$("input.cw-link-target").show();
-            $view.$("select.cw-link-protocol").show();
+        if ($type == 'external') {
+            $view.$('select.cw-link-target').hide();
+            $view.$('input.cw-link-target').show();
+            $view.$('select.cw-link-protocol').show();
         }
     },
 
@@ -91,18 +91,17 @@ export default AuthorView.extend({
 
     onSave(event) {
         var $view = this;
-        var $linktype = $view.$(".cw-link-type option:selected").val();
-        if ($linktype == "internal") {
-            var $linktarget = $view.$("select.cw-link-target option:selected").val();
+        var $linktype = $view.$('.cw-link-type option:selected').val();
+        if ($linktype == 'internal') {
+            var $linktarget = $view.$('select.cw-link-target option:selected').val();
         }
-        if ($linktype == "external") {
-            var $linkprotocol = $view.$("select.cw-link-protocol option:selected").val();
-            var $linktarget = $view.$("input.cw-link-target").val().replace('http://','').replace('https://','');
+        if ($linktype == 'external') {
+            var $linkprotocol = $view.$('select.cw-link-protocol option:selected').val();
+            var $linktarget = $view.$('input.cw-link-target').val().replace('http://','').replace('https://','');
             $linktarget = $linkprotocol+$linktarget;
         }
-        var $linktitle = $view.$(".cw-link-title").val();
-
-        if ($linktitle == "") {
+        var $linktitle = $view.$('.cw-link-title').val();
+        if ($linktitle == '') {
             $linktitle = $linktarget;
         }
 
@@ -124,6 +123,20 @@ export default AuthorView.extend({
                 console.log(error);
             }
         );
+    },
+
+    onChangeLinkTarget(event) {
+        var $view = this,
+        link = $view.$('input.cw-link-target');
+        if (link.val().indexOf('http://') > -1) {
+            link.val(link.val().replace('http://',''));
+            $view.$('select.cw-link-protocol option[value="http://"]').prop('selected', true);
+        }
+        if (link.val().indexOf('https://') > -1) {
+            link.val(link.val().replace('https://',''));
+            $view.$('select.cw-link-protocol option[value="https://"]').prop('selected', true);
+        }
+
     }
 });
     
