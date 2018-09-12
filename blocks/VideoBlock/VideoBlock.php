3e523652ff4e822b6dded88d879033c1b7657e0e
rlucke <rlucke@uni-osnabrueck.de>
2018-09-12 11:48:05 +0100

    enhance VideoBlock

--- blocks/VideoBlock/VideoBlock.php
+++ blocks/VideoBlock/VideoBlock.php
@@ -11,7 +11,7 @@ class VideoBlock extends Block
 {
     const NAME = 'Video';
     const BLOCK_CLASS = 'multimedia';
-    const DESCRIPTION = 'Spielt ein Video einer URL oder YouTube ab';
+    const DESCRIPTION = 'Spielt ein Video aus dem Dateibereich oder von einer URL ab';
 
     public function initialize()
     {
@@ -47,8 +47,14 @@ class VideoBlock extends Block
     public function author_view()
     {
         $this->authorizeUpdate();
+        $video_files = $this->showFiles();
+        if (empty($video_files)) {
+            $video_files = false;
+        }
 
-        return $this->array_rep();
+        return array_merge($this->array_rep(), array(
+            'video_files' => $video_files
+        ));
     }
 
     public function save_handler($data)
@@ -63,6 +69,34 @@ class VideoBlock extends Block
         return $this->array_rep();
     }
 
+    private function showFiles()
+    {
+        $db = \DBManager::get();
+        $stmt = $db->prepare('
+            SELECT 
+                * 
+            FROM 
+                dokumente 
+            WHERE 
+                seminar_id = :seminar_id
+            ORDER BY 
+                name
+        ');
+        $stmt->bindParam(':seminar_id', $this->container['cid']);
+        $stmt->execute();
+        $response = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        $filesarray = array();
+        foreach ($response as $item) {
+            if ((strpos($item['filename'], 'mp4') > -1) || (strpos($item['filename'], 'webm') > -1) || (strpos($item['filename'], 'ogg') > -1)) {
+                $document = \StudipDocument::find($item['dokument_id']);
+                $item['download_url'] = \URLHelper::getURL( 'sendfile.php',  array('type'=>'0', 'file_id' => $document->id, 'file_name' => $document->name) );
+                $filesarray[] = $item;
+            }
+        }
+
+        return $filesarray;
+    }
+
     /**
      * {@inheritdoc}
      */
@@ -71,6 +105,26 @@ class VideoBlock extends Block
         return $this->array_rep();
     }
 
+    public function getFiles()
+    {
+        $sources = json_decode($this->webvideo);
+        foreach ($sources as $source) {
+            if ($source->file_id != '') {
+                $document = new \StudipDocument($source->file_id);
+                $files[] = array(
+                    'id' => $source->file_id,
+                    'name' => $document->name,
+                    'description' => $document->description,
+                    'filename' => $document->filename,
+                    'filesize' => $document->filesize,
+                    'url' => $document->url,
+                    'path' => get_upload_file_path($source->file_id),
+                );
+            }
+        }
+
+        return $files;
+    }
     /**
      * {@inheritdoc}
      */
@@ -111,6 +165,23 @@ class VideoBlock extends Block
         if (isset($properties['videoTitle'])) {
             $this->videoTitle = $properties['videoTitle'];
         }
+
+        $this->save();
+    }
+
+    public function importContents($contents, array $files)
+    {
+        $webvideo = json_decode($this->webvideo);
+        foreach($files as $file){
+            foreach ($webvideo as &$source) {
+                if(($source->file_name == $file->name) && ($source->file_id != '')) {
+                    $source->file_id = $file->id;
+                    $document = \StudipDocument::find($file->id);
+                    $source->src = \URLHelper::getURL( 'sendfile.php',  array('type'=>'0', 'file_id' => $document->id, 'file_name' => $document->name) );
+                }
+            }
+        }
+        $this->webvideo = json_encode($webvideo);
 
         $this->save();
     }
