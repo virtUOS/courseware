5e12b38bfd594fd1a8e4c16efcdca610f73990f9
rlucke <rlucke@uni-osnabrueck.de>
2018-10-05 10:16:56 +0100

    add ScrollyBlock

---
+++ blocks/ScrollyBlock/ScrollyBlock.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Mooc\UI\ScrollyBlock;
+
+use Mooc\UI\Block;
+
+class ScrollyBlock extends Block
+{
+    const NAME = 'Scrolly';
+    const BLOCK_CLASS = 'scrolly';
+    const DESCRIPTION = 'lorem ipsum dolor askimet';
+
+    public function initialize()
+    {
+        $this->defineField('scrolly_block_style', \Mooc\SCOPE_BLOCK, '');
+    }
+
+    public function student_view()
+    {
+        if (!$this->isAuthorized()) {
+            return array('inactive' => true);
+        }
+
+        return array_merge($this->getAttrArray(), array());
+    }
+
+    public function author_view()
+    {
+        $this->authorizeUpdate();
+
+        return array_merge($this->getBlocksInSection(), $this->getAttrArray());
+    }
+
+    public function save_handler(array $data)
+    {
+        $this->authorizeUpdate();
+
+        if (isset($data['scrolly_block_style'])) {
+            $this->scrolly_block_style = (string) $data['scrolly_block_style'];
+        }
+
+        return;
+    }
+
+    private function getBlocksInSection()
+    {
+        $children = $this->getModel()->parent->children;
+        $blocks = array();
+        foreach($children as $child)
+        {
+            if (!in_array($child["type"], array("ScrollyBlock"))){
+                $className = '\Mooc\UI\\'.$child["type"].'\\'.$child["type"];
+                $blocks[] = array('blockid' =>$child->id, 'blocktype'=> $child->type, 'blockname' => _cw(constant($className.'::NAME')));
+            }
+        }
+
+        return array('blocks' => $blocks);
+    }
+
+    private function getAttrArray()
+    {
+        return array(
+            'scrolly_block_style' => $this->scrolly_block_style
+        );
+    }
+
+    public function exportProperties()
+    {
+       return array_merge(
+            $this->getAttrArray(),
+            array('scrolly_block_style' => $this->scrolly_block_style)
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getXmlNamespace()
+    {
+        return 'http://moocip.de/schema/block/scrolly/';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getXmlSchemaLocation()
+    {
+        return 'http://moocip.de/schema/block/scrolly/scrolly-1.0.xsd';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function importProperties(array $properties)
+    {
+        if (isset($properties['scrolly_block_style'])) {
+            $this->scrolly_block_style = $properties['scrolly_block_style'];
+        }
+
+        $this->save();
+    }
+
+}
---
+++ blocks/ScrollyBlock/js/student_view.js
@@ -0,0 +1,45 @@
+import $ from 'jquery'
+import StudentView from 'js/student_view'
+import helper from 'js/url'
+
+export default StudentView.extend({
+    events: {
+    },
+
+    initialize() { },
+    
+    render() {
+    return this;
+    },
+
+    postRender() {
+        var $view = this;
+        try {
+            var block_styles = JSON.parse(this.$('.cw-scrolly-stored-block-style').val());
+
+            $.each(block_styles, function(){
+                switch ((this).style) {
+                    case 'full':
+                    case 'big':
+                    case 'medium':
+                        $('#block-'+(this).blockid).addClass('cw-scrolly-'+(this).style);
+                        break;
+                    case 'left-small':
+                    case 'left-big':
+                    case 'left-medium':
+                    case 'right-small':
+                    case 'right-big':
+                    case 'right-medium':
+                        var element = $('#block-'+(this).blockid);
+                        var position = element.prev().position();
+                        $('#block-'+(this).blockid).addClass('cw-scrolly-'+(this).style).css('top', position.top);
+                        break;
+                    case 'default':
+                        break;
+                }
+            });
+        } catch (err) {
+            //console.log('no scrolly: ' + err);
+        }
+    }
+});
