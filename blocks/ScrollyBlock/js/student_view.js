e1553a82971788652c4b340cc218d748eba3558a
rlucke <rlucke@uni-osnabrueck.de>
2018-11-09 15:06:07 +0100

    enhance and style scrollytelling

--- blocks/ScrollyBlock/js/student_view.js
+++ blocks/ScrollyBlock/js/student_view.js
@@ -14,32 +14,35 @@ export default StudentView.extend({
 
     postRender() {
         var $view = this;
-        try {
-            var block_styles = JSON.parse(this.$('.cw-scrolly-stored-block-style').val());
+        if ($('section#courseware').hasClass('scrollyware')) {
+            try {
+                var block_styles = JSON.parse(this.$('.cw-scrolly-stored-block-style').val());
 
-            $.each(block_styles, function(){
-                switch ((this).style) {
-                    case 'full':
-                    case 'big':
-                    case 'medium':
-                        $('#block-'+(this).blockid).addClass('cw-scrolly-'+(this).style);
-                        break;
-                    case 'left-small':
-                    case 'left-big':
-                    case 'left-medium':
-                    case 'right-small':
-                    case 'right-big':
-                    case 'right-medium':
-                        var element = $('#block-'+(this).blockid);
-                        var position = element.prev().position();
-                        $('#block-'+(this).blockid).addClass('cw-scrolly-'+(this).style).css('top', position.top);
-                        break;
-                    case 'default':
-                        break;
-                }
-            });
-        } catch (err) {
-            //console.log('no scrolly: ' + err);
+                $.each(block_styles, function(){
+                    switch ((this).style) {
+                        case 'full':
+                        case 'big':
+                        case 'medium':
+                            $('#block-'+(this).blockid).addClass('cw-scrolly-'+(this).style);
+                            break;
+                        case 'left-small':
+                        case 'left-big':
+                        case 'left-medium':
+                        case 'right-small':
+                        case 'right-big':
+                        case 'right-medium':
+                            var element = $('#block-'+(this).blockid);
+                            var position = element.prev().position();
+                            $('#block-'+(this).blockid).addClass('cw-scrolly-'+(this).style).css('top', position.top);
+                            break;
+                        case 'default':
+                            break;
+                    }
+                });
+            } catch (err) {
+                //console.log('no scrolly: ' + err);
+            }
+            $(window).trigger('resize');
         }
     }
 });
