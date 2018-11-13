9f5fd2488afee4c45decbeccbb3708d836f61045
rlucke <rlucke@uni-osnabrueck.de>
2018-11-13 13:19:37 +0100

    add DateBlock

---
+++ blocks/DateBlock/DateBlock.php
@@ -0,0 +1,65 @@
+<?php
+namespace Mooc\UI\DateBlock;
+
+use Mooc\UI\Block;
+
+class DateBlock extends Block 
+{
+    const NAME = 'DateBlock';
+    const BLOCK_CLASS = 'layout';
+    const DESCRIPTION = 'DateBlock';
+
+    public function initialize()
+    {
+        $this->defineField('date_content', \Mooc\SCOPE_BLOCK, ''); //JSON
+    }
+
+    public function student_view()
+    {
+        $this->setGrade(1.0);
+
+        return array('date_content' => \STUDIP\Markup::purifyHtml($this->date_content));
+    }
+
+    public function author_view()
+    {
+        $this->authorizeUpdate();
+
+        return array('date_content' => $this->date_content);
+    }
+
+    public function save_handler(array $data)
+    {
+        $this->authorizeUpdate();
+
+        if (isset ($data['date_content'])) {
+            $this->date_content = (string) $data['date_content'];
+        } 
+
+        return;
+    }
+
+    public function exportProperties()
+    {
+       return $this->getAttrArray();
+    }
+
+    public function getXmlNamespace()
+    {
+        return 'http://moocip.de/schema/block/date/';
+    }
+
+    public function getXmlSchemaLocation()
+    {
+        return 'http://moocip.de/schema/block/date/date-1.0.xsd';
+    }
+
+    public function importProperties(array $properties)
+    {
+        if (isset($properties['date_content'])) {
+            $this->date_content = $properties['date_content'];
+        }
+
+        $this->save();
+    }
+}
---
+++ blocks/DateBlock/js/author_view.js
@@ -0,0 +1,84 @@
+import $ from 'jquery'
+import Backbone from 'backbone'
+import AuthorView from 'js/author_view'
+import helper from 'js/url'
+
+export default AuthorView.extend({
+    events: {
+        'click button[name=save]':   'onSave',
+        'click button[name=cancel]': 'switchBack'
+    },
+
+    initialize() {
+        Backbone.on('beforemodeswitch', this.onModeSwitch, this);
+        Backbone.on('beforenavigate', this.onNavigate, this);
+    },
+
+    render() {
+        return this;
+    },
+
+    postRender() {
+        var $view = this;
+
+
+        return this;
+    },
+
+    onNavigate(event) {
+        if (!$('section .block-content button[name=save]').length) {
+            return;
+        }
+        if(event.isUserInputHandled) {
+            return;
+        }
+        event.isUserInputHandled = true;
+        Backbone.trigger('preventnavigateto', !confirm('Es gibt nicht gespeicherte Änderungen. Möchten Sie die Seite trotzdem verlassen?'));
+    },
+
+    onModeSwitch(toView, event) {
+        if (toView != 'student') {
+            return;
+        }
+        // the user already switched back (i.e. the is not visible)
+        if (!this.$el.is(':visible')) {
+            return;
+        }
+        // another listener already handled the user's feedback
+        if (event.isUserInputHandled) {
+            return;
+        }
+        event.isUserInputHandled = true;
+        Backbone.trigger('preventviewswitch', !confirm('Es gibt nicht gespeicherte Änderungen. Möchten Sie trotzdem fortfahren?'));
+    },
+
+    onSave(event) {
+        var $view = this;
+        var content = {};
+        var date = $view.$('.cw-date-countdown-date').val();
+        var time = $view.$('.cw-date-countdown-time').val();
+        
+        content.type = "countdown";
+        content.date = date;
+        content.time = time;
+        
+        content = JSON.stringify(content);
+
+        helper
+        .callHandler(this.model.id, 'save', {
+            'date_content' : content,
+        })
+        .then(
+            // success
+            function () {
+                $(event.target).addClass('accept');
+                $view.switchBack();
+            },
+
+            // error
+            function (error) {
+                console.log(error);
+            }
+        );
+    }
+});
