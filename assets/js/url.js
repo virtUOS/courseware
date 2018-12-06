faa303164c1ba83e8ec9cff3215fd07ee3c037c0
rlucke <rlucke@uni-osnabrueck.de>
2018-12-06 13:00:34 +0100

    fix url params encoding issue

--- assets/js/url.js
+++ assets/js/url.js
@@ -30,7 +30,7 @@ export default {
   },
 
   navigateTo(id, hash) {
-    const params = queryString.parse(queryString.extract(location.href));
+    const params = queryString.parse(queryString.extract(location.href.split("#")[0]));
     params.selected = id;
 
     if (typeof hash === 'undefined' || hash === null) {
