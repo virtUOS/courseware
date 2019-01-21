faa9fa909a6ce3f5f8d97d339f0a15e90343917d
rlucke <rlucke@uni-osnabrueck.de>
2019-01-21 13:31:36 +0100

    CanvasBlock: add Text

--- blocks/CanvasBlock/js/student_view.js
+++ blocks/CanvasBlock/js/student_view.js
@@ -11,7 +11,8 @@ export default StudentView.extend({
         'mouseleave .cw-canvasblock-canvas' :'mouseUp',
         'click .cw-canvasblock-clear': 'clear',
         'click .cw-canvasblock-color': 'changeColor',
-        'click .cw-canvasblock-size': 'changeSize'
+        'click .cw-canvasblock-size': 'changeSize',
+        'click .cw-canvasblock-tool': 'changeTool'
     },
 
     initialize() { },
@@ -22,24 +23,31 @@ export default StudentView.extend({
 
     postRender() {
         var $view = this;
+        var $original_img = $view.$('.cw-canvasblock-original-img');
+        if ($original_img.length < 1) {
+            return;
+        }
         var canvas = $view.$('.cw-canvasblock-canvas')[0];
-        canvas.width = 800;
-        canvas.height = 400;
+        canvas.width = 860;
+        canvas.height = Math.round((canvas.width / $original_img[0].width) * $original_img[0].height);
+        $original_img.hide();
         this.context = canvas.getContext( '2d' );
         this.paint = false;
+        this.write = false;
         this.clickX = new Array();
         this.clickY = new Array();
         this.clickDrag = new Array();
 
         this.colors = {
-                'blue': 'rgba(52,152,219 ,1)',
-                'green': 'rgba(46,204,113 ,1)',
-                'purple': 'rgba(155,89,182 ,1)',
-                'red': 'rgba(231,76,60 ,1)',
-                'yellow': 'rgba(254, 211, 48, 1)',
-                'orange': 'rgba(243,156,18 ,1)',
-                'grey': 'rgba(149,165,166 ,1)',
-                'darkgrey': 'rgba(52,73,94 ,1)'
+                'white': 'rgba(255,255,255,1)',
+                'blue': 'rgba(52,152,219,1)',
+                'green': 'rgba(46,204,113,1)',
+                'purple': 'rgba(155,89,182,1)',
+                'red': 'rgba(231,76,60,1)',
+                'yellow': 'rgba(254,211,48,1)',
+                'orange': 'rgba(243,156,18,1)',
+                'grey': 'rgba(149,165,166,1)',
+                'darkgrey': 'rgba(52,73,94,1)'
         };
         this.clickColor = new Array();
         this.currentColor = this.colors['blue'];
@@ -48,15 +56,30 @@ export default StudentView.extend({
         this.clickSize = new Array();
         this.currentSize = this.sizes['normal'];
 
+        this.tools = {'pen': 'pen', 'text': 'text'}
+        this.clickTool = new Array();
+        this.currentTool = this.tools['pen'];
+
+        this.Text = new Array();
+
         this.redraw();
     },
 
     mouseDown(e) {
+        if (this.write) {
+            return;
+        }
         var mouseX = e.offsetX;
         var mouseY = e.offsetY;
-        this.paint = true;
-        this.addClick(e.offsetX, e.offsetY);
-        this.redraw();
+        if(this.currentTool == 'pen') {
+            this.paint = true;        
+            this.addClick(e.offsetX, e.offsetY);
+            this.redraw();
+        }
+        if(this.currentTool == 'text') {
+            this.write = true;
+            this.addClick(e.offsetX, e.offsetY);
+        }
     },
 
     mouseMove(e) {
@@ -76,6 +99,12 @@ export default StudentView.extend({
         this.clickDrag.push(dragging);
         this.clickColor.push(this.currentColor);
         this.clickSize.push(this.currentSize);
+        this.clickTool.push(this.currentTool);
+        if (this.currentTool == 'text') {
+           this.enableTextInput(x, y);
+        } else {
+            this.Text.push('');
+        }
     },
 
     redraw() {
@@ -86,24 +115,32 @@ export default StudentView.extend({
 
         var outlineImage = new Image();
         outlineImage.src = this.$('.cw-canvasblock-bgimage').val();
-        
+
         context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
         context.drawImage(outlineImage, 0, 0, context.canvas.width, context.canvas.height);
         context.lineJoin = "round";
 
         for(var i=0; i < clickX.length; i++) {
-            context.beginPath();
-            if(clickDrag[i] && i){
-              context.moveTo(clickX[i-1], clickY[i-1]);
-             }else{
-               context.moveTo(clickX[i]-1, clickY[i]);
-             }
-             context.lineTo(clickX[i], clickY[i]);
-             context.closePath();
-             context.strokeStyle = this.clickColor[i];
-             context.lineWidth = this.clickSize[i];
-             context.stroke();
-             
+            if (this.clickTool[i] == 'pen') {
+                context.beginPath();
+                if(clickDrag[i] && i) {
+                    context.moveTo(clickX[i-1], clickY[i-1]);
+                 } else {
+                     context.moveTo(clickX[i]-1, clickY[i]);
+                 }
+                 context.lineTo(clickX[i], clickY[i]);
+                 context.closePath();
+                 context.strokeStyle = this.clickColor[i];
+                 context.lineWidth = this.clickSize[i];
+                 context.stroke();
+            }
+            if (this.clickTool[i] == 'text') {
+                context.font = this.clickSize[i]*6+"px Arial";
+            console.log(context.font);
+                context.fillStyle = this.clickColor[i];
+                context.fillText(this.Text[i], clickX[i], clickY[i]); 
+                
+            }
         }
     },
 
@@ -113,6 +150,8 @@ export default StudentView.extend({
         this.clickDrag.length = 0;
         this.clickColor.length = 0;
         this.clickSize.length = 0;
+        this.clickTool.length = 0;
+        this.Text.length = 0;
         this.redraw();
     },
 
@@ -124,5 +163,31 @@ export default StudentView.extend({
     changeSize(e) {
         var size = e.target.value;
         this.currentSize = this.sizes[size];
+    },
+
+    changeTool(e) {
+        var tool = e.target.value;
+        this.currentTool = this.tools[tool];
+    },
+
+    enableTextInput(x, y) {
+        var $view = this;
+        this.$('input.cw-canvasblock-text-input').remove();
+        this.$el.append('<input class="cw-canvasblock-text-input">');
+        var $input = this.$('input.cw-canvasblock-text-input');
+        $input.css('position', 'absolute');
+        $input.css('top',  (this.$('canvas')[0].offsetTop + y) + 'px');
+        $input.css('left',  x +'px');
+
+        $input[0].addEventListener('keypress', function(e){
+            var key = e.which || e.keyCode;
+            if (key === 13) { // 13 is enter
+                $view.Text.push($input.val());
+                $view.$('input.cw-canvasblock-text-input').remove();
+                $view.write = false;
+                $view.redraw();
+            }
+        }, false);
+
     }
 });
