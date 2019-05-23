78ecd7bc1ee2f667f31fb879751e91aa15ccb30c
Ron Lucke <rlucke@uos.de>
2019-05-23 10:48:17 +0100

    GalleryBlock bugfix: keep image aspekt

--- blocks/GalleryBlock/js/student_view.js
+++ blocks/GalleryBlock/js/student_view.js
@@ -14,13 +14,25 @@ export default StudentView.extend({
   },
 
   postRender() {
-      var $autoplay = this.$el.find('input[name="gallery-autoplay"]').val() == 1;
-      var $autoplaytimer = this.$el.find('input[name="gallery-autoplay-timer"]').val()*1000;
-      var $hidenav = this.$el.find('input[name="gallery-hidenav"]').val() == 0;
+      let $autoplay = this.$('input[name="gallery-autoplay"]').val() == 1;
+      let $autoplaytimer = this.$('input[name="gallery-autoplay-timer"]').val()*1000;
+      let $hidenav = this.$('input[name="gallery-hidenav"]').val() == 0;
+      let gallery_height = this.$('input[name="gallery-height"]').val();
+      let gallery_width = this.$('.cw-gallery-image-wrapper')[0].offsetWidth;
+      let $images = this.$('.cw-gallery-image');
       if (!$hidenav) {
             this.$el.find(".cw-gallery").addClass("cw-gallery-hidenav");
       }
+      $.each($images, function(){
+        let img = $(this)[0];
+        let aspact = img.width / img.height;
+        img.height = gallery_width / aspact;
+        if (img.height > gallery_height) {
+          img.height = gallery_height;
+        }
+      });
       $autoplaytimer == "" ? $autoplaytimer = 2000 : $autoplaytimer = $autoplaytimer;
+      
       this.$('.cw-gallery').slick({
             arrows: $hidenav,
             autoplay: $autoplay,
