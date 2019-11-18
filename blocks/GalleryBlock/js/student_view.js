36b8813f35a65b367bad02c361aec625870d66f0
rlucke <rlucke@uni-osnabrueck.de>
2019-11-18 16:05:46 +0100

    enhance GalleryBlock

--- blocks/GalleryBlock/js/student_view.js
+++ blocks/GalleryBlock/js/student_view.js
@@ -1,6 +1,6 @@
-import jQuery from 'jquery'
-import StudentView from 'js/student_view'
-import slick from 'slick-carousel'
+import jQuery from 'jquery';
+import StudentView from 'js/student_view';
+import slick from 'slick-carousel';
 
 
 export default StudentView.extend({
@@ -14,33 +14,43 @@ export default StudentView.extend({
   },
 
   postRender() {
-      if(this.$('.messagebox_info').length) {
-        return;
-      }
-      let $autoplay = this.$('input[name="gallery-autoplay"]').val() == 1;
-      let $autoplaytimer = this.$('input[name="gallery-autoplay-timer"]').val()*1000;
-      let $hidenav = this.$('input[name="gallery-hidenav"]').val() == 0;
-      let gallery_height = this.$('input[name="gallery-height"]').val();
-      let gallery_width = this.$('.cw-gallery-image-wrapper')[0].offsetWidth;
-      let $images = this.$('.cw-gallery-image');
-      if (!$hidenav) {
-            this.$el.find(".cw-gallery").addClass("cw-gallery-hidenav");
-      }
-      $.each($images, function(){
-        let img = $(this)[0];
-        let aspact = img.width / img.height;
-        img.height = gallery_width / aspact;
-        if (img.height > gallery_height) {
+    if(this.$('input[name="gallery_has_files"]').val() == 0) {
+      return;
+    }
+    let view = this;
+    let $autoplay = this.$('input[name="gallery-autoplay"]').val() == 1;
+    let $autoplaytimer = this.$('input[name="gallery-autoplay-timer"]').val() * 1000;
+    let $hidenav = this.$('input[name="gallery-hidenav"]').val() == 0;
+    let gallery_height = this.$('input[name="gallery-height"]').val();
+    let gallery_width = this.$('.cw-gallery-image-wrapper')[0].offsetWidth;
+    let $images = this.$('.cw-gallery-image');
+    if (!$hidenav) {
+      this.$el.find('.cw-gallery').addClass('cw-gallery-hidenav');
+    }
+    $images.ready(function () {
+      jQuery.each($images, function () {
+        let img = jQuery(this)[0];
+        let aspect = img.width / img.height;
+        if (img.height == 0) {
           img.height = gallery_height;
+        } else {
+          img.height = gallery_width / aspect;
+          if (img.height > gallery_height) {
+            img.height = gallery_height;
+          }
         }
       });
-      $autoplaytimer == "" ? $autoplaytimer = 2000 : $autoplaytimer = $autoplaytimer;
-      
-      this.$('.cw-gallery').slick({
-            arrows: $hidenav,
-            autoplay: $autoplay,
-            autoplaySpeed: $autoplaytimer,
-            infinite: true
+      if ($autoplaytimer == '') {
+        $autoplaytimer = 2000;
+      }
+      view.$('.cw-gallery').slick({
+        arrows: $hidenav,
+        autoplay: $autoplay,
+        autoplaySpeed: $autoplaytimer,
+        infinite: true
       });
+      view.$('.cw-gallery').show();
+    });
+
   }
 });
