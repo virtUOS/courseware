12270b3b5fd522e40ad985ca81c0f10c3618bc38
Till Glöggler <till@gundk.it>
2020-06-26 15:01:57 +0200

    fix migration

--- migrations/011_add_read_write_perms.php
+++ migrations/011_add_read_write_perms.php
@@ -27,18 +27,16 @@ class AddReadWritePerms extends Migration
             $new_app = [];
             $app = json_decode($data['approval'], true);
 
-            if ($app['users']
-                && !$app['users']['read']
-                && !$app['users']['write']
-            ) {
-                $new_app['users'][$app['users']] = 'write';
+            if ($app['users']) {
+                foreach ($app['users'] as $id) {
+                    $new_app['users'][$id] = 'write';
+                }
             }
 
-            if ($app['groups']
-                && !$app['groups']['read']
-                && !$app['groups']['write']
-            ) {
-                $new_app['groups'][$app['groups']] = 'write';
+            if ($app['groups']) {
+                foreach ($app['groups'] as $id) {
+                    $new_app['groups'][$id] = 'write';
+                }
             }
 
             if (!empty($new_app)) {
