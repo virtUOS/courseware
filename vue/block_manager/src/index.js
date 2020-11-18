8a7abaa81a0a4cda78e7ccae4facc182d06e8a05
mlunzena <mlunzena@uos.de>
2020-11-18 07:53:21 +0100

    fix missing i18n and vuex caused by vuedraggable

--- vue/block_manager/src/index.js
+++ vue/block_manager/src/index.js
@@ -1,13 +1,19 @@
 import Vue from 'vue';
-import BlockManager from './BlockManager.vue';
 import store from './store';
 import i18n from './i18n';
 
 Vue.config.productionTip = false;
 
-new Vue({
+const draggableHack = window.Vue;
+delete window.Vue;
+
+import('./BlockManager.vue').then(({ default: BlockManager}) => {
+  new Vue({
     el: '#block_mananger_content',
     i18n,
     store,
     render: h => h(BlockManager)
+  });
+
+  window.Vue = draggableHack;
 });
