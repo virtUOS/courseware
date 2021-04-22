6b4bf99c5c9cf7e2090302e1fcee5f95efb16eac
\nrlucke <rlucke@uos.de>
2021-04-22 10:36:34 +0100

    update dependencies

--- views/block_manager/index.php
+++ views/block_manager/index.php
@@ -1,4 +1,4 @@
-<head><link href=../../../plugins_packages/virtUOS/Courseware/views/block_manager/../../assets/vue/css/chunk-vendors.css rel=preload as=style><link href=../../../plugins_packages/virtUOS/Courseware/views/block_manager/../../assets/vue/js/chunk-vendors.js rel=preload as=script><link href=../../../plugins_packages/virtUOS/Courseware/views/block_manager/../../assets/vue/js/index.js rel=preload as=script><link href=../../../plugins_packages/virtUOS/Courseware/views/block_manager/../../assets/vue/css/chunk-vendors.css rel=stylesheet></head><noscript><strong>We're sorry but block_mananger doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript><script>const COURSEWARE = {
+<head><link href="../../../plugins_packages/virtUOS/Courseware/views/block_manager/../../assets/vue/css/chunk-vendors.css" rel="preload" as="style"><link href="../../../plugins_packages/virtUOS/Courseware/views/block_manager/../../assets/vue/js/chunk-vendors.js" rel="preload" as="script"><link href="../../../plugins_packages/virtUOS/Courseware/views/block_manager/../../assets/vue/js/index.js" rel="preload" as="script"><link href="../../../plugins_packages/virtUOS/Courseware/views/block_manager/../../assets/vue/css/chunk-vendors.css" rel="stylesheet"></head><noscript><strong>We're sorry but block_mananger doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript><script>const COURSEWARE = {
         config: {
             cid: <?=json_encode($cid)?> },
         data: {
@@ -10,4 +10,4 @@
             courseware_import_url: '<?= $courseware_import_url ?>',
             courseware_export_html_url: '<?= $courseware_export_html_url ?>'
         }
-    }</script><div id=block_mananger_content></div><script src=../../../plugins_packages/virtUOS/Courseware/views/block_manager/../../assets/vue/js/chunk-vendors.js></script><script src=../../../plugins_packages/virtUOS/Courseware/views/block_manager/../../assets/vue/js/index.js></script>
+    }</script><div id="block_mananger_content"></div><script src="../../../plugins_packages/virtUOS/Courseware/views/block_manager/../../assets/vue/js/chunk-vendors.js"></script><script src="../../../plugins_packages/virtUOS/Courseware/views/block_manager/../../assets/vue/js/index.js"></script>
