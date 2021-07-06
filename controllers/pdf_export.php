8ffb52beb9bd3700f9cdfc57f25e72c215f6b689
mlunzena <luniki@gmail.com>
2021-07-06 06:00:21 +0200

    add PDF export button, fixes #257

---
+++ controllers/pdf_export.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * PDF Exporter
+ *
+ * @author <lunzenauer@elan-ev.de>
+ */
+class PdfExportController extends CoursewareStudipController
+{
+    public function index_action()
+    {
+    }
+}
